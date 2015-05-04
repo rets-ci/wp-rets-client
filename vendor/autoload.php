6bd2da89211ad8b56d41be9300ebd6dab36d98ae
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-05-04 20:06:24 +0300

    n/a

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInita628c1366f83bc2328da084bd86aff9f::getLoader();
+return ComposerAutoloaderInit6f07dc39cb2d2377b285adc038c42114::getLoader();
