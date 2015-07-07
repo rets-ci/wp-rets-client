df79d51397a6211d538700ef3b8cf57994c6807b
maria.kravchenko <maria.kravchenko@usabilitydynamics.com>
2015-07-07 13:36:20 +0000

    maria.kravchenko installed unknown unknown

---
+++ wp-content/plugins/wp-property-feps/lib/classes/class-feps-bootstrap.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Bootstrap
+ *
+ * @since 3.0.0
+ */
+namespace UsabilityDynamics\WPP {
+
+  if( !class_exists( 'UsabilityDynamics\WPP\FEPS_Bootstrap' ) ) {
+
+    final class FEPS_Bootstrap extends \UsabilityDynamics\WP\Bootstrap {
+      
+      /**
+       * Singleton Instance Reference.
+       *
+       * @protected
+       * @static
+       * @property $instance
+       * @type UsabilityDynamics\WPP\FEPS_Bootstrap object
+       */
+      protected static $instance = null;
+      
+      /**
+       * Instantaite class.
+       */
+      public function init() {
+        //** Be sure we do not have errors. Do not initialize plugin if we have them. */
+        if( $this->has_errors() ) {
+          return null;
+        }
+        
+        require_once( dirname( __DIR__ ) . '/class-wpp-feps.php' );
+        add_action( 'wpp_pre_init', array( 'class_wpp_feps', 'wpp_pre_init' ) );
+        add_action( 'wpp_post_init', array( 'class_wpp_feps', 'wpp_post_init' ) );
+        add_action( 'widgets_init', create_function( '', 'return register_widget("MyFepsWidget");' ) );
+        add_action( 'widgets_init', create_function( '', 'return register_widget("MyFepsInfoWidget");' ) );
+      }
+      
+      /**
+       * Plugin Activation
+       *
+       */
+      public function activate() {}
+      
+      /**
+       * Plugin Deactivation
+       *
+       */
+      public function deactivate() {}
+
+    }
+
+  }
+
+}
