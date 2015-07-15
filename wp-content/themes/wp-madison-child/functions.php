4ff205fef25f6fe5244d40dabe5f0c1cef5147dc
Anton Korotkov <antonkorotkoff@gmail.com>
2015-07-15 15:53:16 +0300

    wip header secondary menu

--- wp-content/themes/wp-madison-child/functions.php
+++ wp-content/themes/wp-madison-child/functions.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * RDC child theme based on WP-Madison 2.0
+ */
 
 /**
- * Enqueue parent style
+ * Enqueue parent style hook
  */
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 /**
- *
+ * Enqueue parent style
  */
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+/**
+ * Register additional nav menu
+ */
+register_nav_menu( 'header-secondary', __('Secondary Header Menu') );
+
