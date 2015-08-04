3218eb362e7b97cb71c4e019a63719650fc9986e
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-08-04 18:32:51 +0300

    added scaffold

---
+++ lib/classes/class-WS_Bootstrap.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Bootstrap
+ *
+ * @since 1.0.0
+ */
+namespace UsabilityDynamics\WPP {
+
+  if( !class_exists( 'UsabilityDynamics\WPP\WS_Bootstrap' ) ) {
+
+    final class WS_Bootstrap extends \UsabilityDynamics\WP\Bootstrap_Plugin {
+      
+      /**
+       * Singleton Instance Reference.
+       *
+       * @protected
+       * @static
+       * @property $instance
+       * @type UsabilityDynamics\WPP\WS_Bootstrap object
+       */
+      protected static $instance = null;
+      
+      /**
+       * Instantaite class.
+       */
+      public function init() {
+        
+        //** Here is we go. */
+        
+      }
+      
+      /**
+       * Plugin Activation
+       *
+       */
+      public function activate() {}
+      
+      /**
+       * Plugin Deactivation
+       *
+       */
+      public function deactivate() {}
+
+    }
+
+  }
+
+}
