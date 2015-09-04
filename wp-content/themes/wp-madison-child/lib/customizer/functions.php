03d9fd16d955841c0761b3e90e1c3c2b8607162f
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-09-04 17:20:58 +0300

    implemented new theme header

--- wp-content/themes/wp-madison-child/lib/customizer/functions.php
+++ wp-content/themes/wp-madison-child/lib/customizer/functions.php
@@ -5,3 +5,28 @@
  */
 
 
+/**
+ * Add custom options to the theme customizer.
+ */
+function rdc_customize_register( $wp_customize ) {
+
+  $wp_customize->add_section( 'rdc_search', array(
+    'title'       => __( 'Multipurpose Search', 'madison' ),
+    'description' => __( 'Control a multipurpose search form in a header.', 'madison' ),
+    'priority'    => 61
+  ));
+
+  $wp_customize->add_setting( 'madison_multipurpose_search_enable', array(
+    'default'              => ''
+  ));
+
+  $wp_customize->add_control( 'madison_multipurpose_search_enable', array(
+    'label'   => __( 'Enable Multipurpose Search', 'madison' ),
+    'section' => 'rdc_search',
+    'type'    => 'checkbox',
+    'settings' => 'madison_multipurpose_search_enable'
+  ));
+
+}
+
+add_action( 'customize_register', 'rdc_customize_register' );
--- wp-content/themes/wp-madison-child/lib/functions.php
+++ wp-content/themes/wp-madison-child/lib/functions.php
@@ -6,13 +6,10 @@ if ( !function_exists( 'madison_maybe_header_multipurpose_search' ) ) {
   /**
    * Display multipurpose search
    */
-  function madison_maybe_header_multipurpose_search() {
+  function madison_maybe_header_multipurpose_search( $classes = 'col-5-12' ) {
     global $wp_query;
 
     $search_enabled = get_theme_mod( 'madison_multipurpose_search_enable' );
-    if( empty( $search_enabled ) ) {
-      //return;
-    }
 
     if( !empty( $wp_query->wpp_search_page ) ) {
       $s = !empty( $_REQUEST['wpp_search']['s'] ) ? $_REQUEST['wpp_search']['s'] : '';
@@ -20,18 +17,28 @@ if ( !function_exists( 'madison_maybe_header_multipurpose_search' ) ) {
       $s = !empty( $_REQUEST['s'] ) ? $_REQUEST['s'] : '';
     }
 
+    if( is_array( $classes ) ) {
+      $classes = implode(' ', $classes );
+    }
+
     ob_start();
 
     ?>
-    <section class="column col-5-12 multipurpose-search-wrapper">
-      <form name="multipurpose_search" action="<?php echo home_url(); ?>" >
-        <input type="hidden" name="mps" value="1" />
-        <input type="text" name="s" value="<?php echo !empty( $s ) ? $s : ''; ?>" placeholder="<?php _e( 'Type Keywords', 'madison' ) ?>" />
-        <select name="post_type">
-          <option value="property"><?php _e( 'Rentals', 'madison' ); ?></option>
-          <option value="post" <?php echo ( !empty( $s ) && !isset( $wp_query->wpp_search_page ) ? 'selected="selected"' : ''); ?>><?php _e( 'Articles', 'madison' ); ?></option>
-        </select>
-      </form>
+    <section class="column multipurpose-search-wrapper <?php echo $classes; ?>">
+      <?php if( !empty( $search_enabled ) ) : ?>
+        <form name="multipurpose_search" action="<?php echo home_url(); ?>">
+          <input type="hidden" name="mps" value="1"/>
+          <input type="text" name="s" value="<?php echo !empty($s) ? $s : ''; ?>"
+                 placeholder="<?php _e('Type Keywords', 'madison') ?>"/>
+          <select name="post_type">
+            <option value="property"><?php _e('Rentals', 'madison'); ?></option>
+            <option
+              value="post" <?php echo(!empty($s) && !isset($wp_query->wpp_search_page) ? 'selected="selected"' : ''); ?>><?php _e('Articles', 'madison'); ?></option>
+          </select>
+        </form>
+      <?php else : ?>
+        &nbsp;
+      <?php endif; ?>
     </section>
     <?php
 
