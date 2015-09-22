c9d7e6210acd0a140a954abb8672991ac619adb3
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-09-22 18:49:31 +0300

    css improvements for feedback form

--- wp-content/themes/wp-madison-child/static/views/rdc-feedback-form.php
+++ wp-content/themes/wp-madison-child/static/views/rdc-feedback-form.php
@@ -1,206 +1,208 @@
-<script src="https://cloud.crm.powerobjects.net/powerWebFormV3/scripts/jquery-1.9.0.validate.min.js" type="text/javascript"></script>
-<form id="powf_E1FC2444265AE411B3136C3BE5A87DF0" class="contact_form"
-      enctype="multipart/form-data"
-      action="https://cloud.crm.powerobjects.net/powerWebFormV3/PowerWebFormData.aspx?t=CCGr6i%2b2CU2A1Z%2bLiVlRh28AcgBnADAANQAyADkANwBlAGYAZAA%3d&formId=powf_E1FC2444265AE411B3136C3BE5A87DF0&tver=2013&c=1"
-      method="post">
+<div class="rdc-feedback-form">
+  <script src="https://cloud.crm.powerobjects.net/powerWebFormV3/scripts/jquery-1.9.0.validate.min.js" type="text/javascript"></script>
+  <form id="powf_E1FC2444265AE411B3136C3BE5A87DF0" class="contact_form"
+        enctype="multipart/form-data"
+        action="https://cloud.crm.powerobjects.net/powerWebFormV3/PowerWebFormData.aspx?t=CCGr6i%2b2CU2A1Z%2bLiVlRh28AcgBnADAANQAyADkANwBlAGYAZAA%3d&formId=powf_E1FC2444265AE411B3136C3BE5A87DF0&tver=2013&c=1"
+        method="post">
 
 
-  <div class="label">
-    <label for="powf_29adbc5e265ae411b3136c3be5a87df0">E-mail</label>
-    <span class="required">*</span>
-    <span class="tip"></span>
+    <div class="label">
+      <label for="powf_29adbc5e265ae411b3136c3be5a87df0">E-mail</label>
+      <span class="required">*</span>
+      <span class="tip"></span>
 
-  </div>
-
-  <div class="field">
-    <input requred type="text" id="powf_29adbc5e265ae411b3136c3be5a87df0" name="powf_29adbc5e265ae411b3136c3be5a87df0"
-           value="" maxlength="100" class="required"/>
-
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_19ae158a265ae411b3136c3be5a87df0">First Name</label>
-    <span class="required">*</span>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <input requred type="text" id="powf_19ae158a265ae411b3136c3be5a87df0" name="powf_19ae158a265ae411b3136c3be5a87df0"
-           value="" maxlength="100" class="required"/>
-
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_2940c89e265ae411b3136c3be5a87df0">Last Name</label>
-    <span class="required">*</span>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <input requred type="text" id="powf_2940c89e265ae411b3136c3be5a87df0" name="powf_2940c89e265ae411b3136c3be5a87df0"
-           value="" maxlength="100" class="required"/>
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_ee9450b8265ae411b3136c3be5a87df0">Company</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <input type="text" id="powf_ee9450b8265ae411b3136c3be5a87df0" name="powf_ee9450b8265ae411b3136c3be5a87df0"
-           value="" maxlength="100"/>
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_9ec8f6d0265ae411b3136c3be5a87df0">Phone Number</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <input type="text" id="powf_9ec8f6d0265ae411b3136c3be5a87df0" name="powf_9ec8f6d0265ae411b3136c3be5a87df0"
-           value="" maxlength="100"/>
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_1d523a4e275ae411b3136c3be5a87df0">Were you working with a broker?</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <select id="powf_1d523a4e275ae411b3136c3be5a87df0" name="powf_1d523a4e275ae411b3136c3be5a87df0">
-      <option value=""></option>
-      <option value="Paul Inserra">Paul Inserra</option>
-      <option value="Bennett Robinson">Bennett Robinson</option>
-      <option value="Judi Reuwer">Judi Reuwer</option>
-      <option value="Kathy Arsena">Kathy Arsena</option>
-      <option value="Susan Halverson ">Susan Halverson</option>
-      <option value="Talina Bailey">Talina Bailey</option>
-      <option value="Todd L. Crenshaw">Todd L. Crenshaw</option>
-      <option value="Germany McNeal">Germany McNeal</option>
-      <option value="Greg DeWitt">Greg DeWitt</option>
-    </select>
-
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_13c3d4a2275ae411b3136c3be5a87df0">What best describes the nature of your
-      comments?</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <select id="powf_13c3d4a2275ae411b3136c3be5a87df0" name="powf_13c3d4a2275ae411b3136c3be5a87df0">
-      <option value=""></option>
-      <option value="Communication">Communication</option>
-      <option value="Timeliness">Timeliness</option>
-      <option value="Attentiveness">Attentiveness</option>
-      <option value="Helpfulness">Helpfulness</option>
-      <option value="Professionalism">Professionalism</option>
-      <option value="General Feedback">General Feedback</option>
-      <option value="Other">Other</option>
-    </select>
-
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_f91ecac0275ae411b3136c3be5a87df0">Comments</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <input type="text" id="powf_f91ecac0275ae411b3136c3be5a87df0" name="powf_f91ecac0275ae411b3136c3be5a87df0"
-           value="" maxlength="2000"/>
-
-  </div>
-
-  <div class="clear"></div>
-  <div class="label">
-    <label for="powf_cae574ef275ae411b3136c3be5a87df0">Please rate your overall experience with Red Door
-      Company.</label>
-    <span class="tip"></span>
-
-  </div>
-
-  <div class="field">
-    <div class="radio_row">
-      <input id="powf_cae574ef275ae411b3136c3be5a87df00" name="powf_cae574ef275ae411b3136c3be5a87df0"
-             type="radio" value="N/A"/><label for="powf_cae574ef275ae411b3136c3be5a87df00" class="lab_radio">N/A</label>
     </div>
-    <div class="radio_row">
-      <input id="powf_cae574ef275ae411b3136c3be5a87df01" name="powf_cae574ef275ae411b3136c3be5a87df0"
-             type="radio" value="Below Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df01"
-                                                             class="lab_radio">Below Expectations</label>
+
+    <div class="field">
+      <input requred type="text" id="powf_29adbc5e265ae411b3136c3be5a87df0" name="powf_29adbc5e265ae411b3136c3be5a87df0"
+             value="" maxlength="100" class="required"/>
+
+
     </div>
-    <div class="radio_row">
-      <input id="powf_cae574ef275ae411b3136c3be5a87df02" name="powf_cae574ef275ae411b3136c3be5a87df0"
-             type="radio" value="Met Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df02"
-                                                           class="lab_radio">Met Expectations</label>
-    </div>
-    <div class="radio_row">
-      <input id="powf_cae574ef275ae411b3136c3be5a87df03" name="powf_cae574ef275ae411b3136c3be5a87df0"
-             type="radio" value="Exceeds Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df03"
-                                                               class="lab_radio">Exceeds Expectations</label>
-    </div>
+
     <div class="clear"></div>
-  </div>
+    <div class="label">
+      <label for="powf_19ae158a265ae411b3136c3be5a87df0">First Name</label>
+      <span class="required">*</span>
+      <span class="tip"></span>
 
-  <div class="clear"></div>
-  <!-- tver -->
-  <input type="hidden" id="tver" name="tver" value="2013"/>
-  <input type="hidden" name="ignore_submitmessage" value=""/>
-  <input type="hidden" name="ignore_linkbuttontext" value=""/>
-  <input type="hidden" name="ignore_redirecturl" value="http://www.reddoorcompany.com/form-submitted/"/>
-  <input type="hidden" name="ignore_redirectmode" value="Auto"/>
+    </div>
 
-  <div class="submit-wrapper">
-    <input class="button" type="submit" value="Send" />
-  </div>
-</form>
+    <div class="field">
+      <input requred type="text" id="powf_19ae158a265ae411b3136c3be5a87df0" name="powf_19ae158a265ae411b3136c3be5a87df0"
+             value="" maxlength="100" class="required"/>
 
-<script type="text/javascript">
-  jQuery(document).ready(function () {
-    jQuery.extend(jQuery.validator.messages, {
 
-      email:"Please enter a valid email address. Make sure there are no leading or trailing spaces."
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_2940c89e265ae411b3136c3be5a87df0">Last Name</label>
+      <span class="required">*</span>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <input requred type="text" id="powf_2940c89e265ae411b3136c3be5a87df0" name="powf_2940c89e265ae411b3136c3be5a87df0"
+             value="" maxlength="100" class="required"/>
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_ee9450b8265ae411b3136c3be5a87df0">Company</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <input type="text" id="powf_ee9450b8265ae411b3136c3be5a87df0" name="powf_ee9450b8265ae411b3136c3be5a87df0"
+             value="" maxlength="100"/>
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_9ec8f6d0265ae411b3136c3be5a87df0">Phone Number</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <input type="text" id="powf_9ec8f6d0265ae411b3136c3be5a87df0" name="powf_9ec8f6d0265ae411b3136c3be5a87df0"
+             value="" maxlength="100"/>
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_1d523a4e275ae411b3136c3be5a87df0">Were you working with a broker?</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <select id="powf_1d523a4e275ae411b3136c3be5a87df0" name="powf_1d523a4e275ae411b3136c3be5a87df0">
+        <option value=""></option>
+        <option value="Paul Inserra">Paul Inserra</option>
+        <option value="Bennett Robinson">Bennett Robinson</option>
+        <option value="Judi Reuwer">Judi Reuwer</option>
+        <option value="Kathy Arsena">Kathy Arsena</option>
+        <option value="Susan Halverson ">Susan Halverson</option>
+        <option value="Talina Bailey">Talina Bailey</option>
+        <option value="Todd L. Crenshaw">Todd L. Crenshaw</option>
+        <option value="Germany McNeal">Germany McNeal</option>
+        <option value="Greg DeWitt">Greg DeWitt</option>
+      </select>
+
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_13c3d4a2275ae411b3136c3be5a87df0">What best describes the nature of your
+        comments?</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <select id="powf_13c3d4a2275ae411b3136c3be5a87df0" name="powf_13c3d4a2275ae411b3136c3be5a87df0">
+        <option value=""></option>
+        <option value="Communication">Communication</option>
+        <option value="Timeliness">Timeliness</option>
+        <option value="Attentiveness">Attentiveness</option>
+        <option value="Helpfulness">Helpfulness</option>
+        <option value="Professionalism">Professionalism</option>
+        <option value="General Feedback">General Feedback</option>
+        <option value="Other">Other</option>
+      </select>
+
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_f91ecac0275ae411b3136c3be5a87df0">Comments</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <input type="text" id="powf_f91ecac0275ae411b3136c3be5a87df0" name="powf_f91ecac0275ae411b3136c3be5a87df0"
+             value="" maxlength="2000"/>
+
+    </div>
+
+    <div class="clear"></div>
+    <div class="label">
+      <label for="powf_cae574ef275ae411b3136c3be5a87df0">Please rate your overall experience with Red Door
+        Company.</label>
+      <span class="tip"></span>
+
+    </div>
+
+    <div class="field">
+      <div class="radio_row">
+        <input id="powf_cae574ef275ae411b3136c3be5a87df00" name="powf_cae574ef275ae411b3136c3be5a87df0"
+               type="radio" value="N/A"/><label for="powf_cae574ef275ae411b3136c3be5a87df00" class="lab_radio">N/A</label>
+      </div>
+      <div class="radio_row">
+        <input id="powf_cae574ef275ae411b3136c3be5a87df01" name="powf_cae574ef275ae411b3136c3be5a87df0"
+               type="radio" value="Below Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df01"
+                                                               class="lab_radio">Below Expectations</label>
+      </div>
+      <div class="radio_row">
+        <input id="powf_cae574ef275ae411b3136c3be5a87df02" name="powf_cae574ef275ae411b3136c3be5a87df0"
+               type="radio" value="Met Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df02"
+                                                             class="lab_radio">Met Expectations</label>
+      </div>
+      <div class="radio_row">
+        <input id="powf_cae574ef275ae411b3136c3be5a87df03" name="powf_cae574ef275ae411b3136c3be5a87df0"
+               type="radio" value="Exceeds Expectations"/><label for="powf_cae574ef275ae411b3136c3be5a87df03"
+                                                                 class="lab_radio">Exceeds Expectations</label>
+      </div>
+      <div class="clear"></div>
+    </div>
+
+    <div class="clear"></div>
+    <!-- tver -->
+    <input type="hidden" id="tver" name="tver" value="2013"/>
+    <input type="hidden" name="ignore_submitmessage" value=""/>
+    <input type="hidden" name="ignore_linkbuttontext" value=""/>
+    <input type="hidden" name="ignore_redirecturl" value="http://www.reddoorcompany.com/form-submitted/"/>
+    <input type="hidden" name="ignore_redirectmode" value="Auto"/>
+
+    <div class="submit-wrapper">
+      <input class="button" type="submit" value="Send" />
+    </div>
+  </form>
+
+  <script type="text/javascript">
+    jQuery(document).ready(function () {
+      jQuery.extend(jQuery.validator.messages, {
+
+        email:"Please enter a valid email address. Make sure there are no leading or trailing spaces."
+      });
+
+      jQuery("#powf_E1FC2444265AE411B3136C3BE5A87DF0").validate({
+        errorPlacement: function(error, element) {
+          error.appendTo( element.parents("div.field:first").find("div.clear:first") );
+        },
+
+        invalidHandler: function(event, validator) {
+          var errors = validator.numberOfInvalids();
+          if (errors) {
+            jQuery("input[type=submit]").removeAttr("disabled");
+          }
+        },
+        onfocusout: false,
+        onkeyup: false,
+        onclick: false,
+        debug: false
+      });
     });
 
-    jQuery("#powf_E1FC2444265AE411B3136C3BE5A87DF0").validate({
-      errorPlacement: function(error, element) {
-        error.appendTo( element.parents("div.field:first").find("div.clear:first") );
-      },
-
-      invalidHandler: function(event, validator) {
-        var errors = validator.numberOfInvalids();
-        if (errors) {
-          jQuery("input[type=submit]").removeAttr("disabled");
-        }
-      },
-      onfocusout: false,
-      onkeyup: false,
-      onclick: false,
-      debug: false
-    });
-  });
-
-</script>
+  </script>
+</div>
