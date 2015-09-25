952822a35f24854d25bbbca8c5c7ff9df9868a47
Andy Potanin <andy@udx.io>
2015-09-25 18:23:02 +0000

    updated plugins and ignores.

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:7:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:160:"dd30d881a9bcad7910374a534e25ab3c45800834aa5e0b00528188b67b570c1a8efa98c48507ceca0e10dac4c1b364fe651e2ed2eb835c5f4237f98a0c9360dcef4e72de9456a76631da6e8bd59f3d3b";s:20:"loginSec_lockoutMins";s:3:"240";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";}
