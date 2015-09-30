ac6efcb6bb0bcec5e5f62658de2a81db78767f0c
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-09-30 15:17:02 +0300

    updated bootstrap library

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit14b86a5a844eccc0b85c71735026eb0a::getLoader();
+return ComposerAutoloaderInitf8854c860b3500f13227f57b61f8e7dc::getLoader();
