cf0bcee41eb306826ede1c629a20f049760ce8c0
Andy Potanin <andy.potanin@usabilitydynamics.com>
2015-11-04 05:14:15 +0700

    changing https redirection to use 301

--- wp-content/mu-plugins/shameless-hacks.php
+++ wp-content/mu-plugins/shameless-hacks.php
@@ -3,8 +3,9 @@
  * Shameless Hacks
  */
 
-// Redirect to https mode.
+// Redirect to https mode.c
 if( empty($_SERVER['HTTPS']) ) {
+  header( "HTTP/1.1 301 Moved Permanently" );
   header( 'Cache-Control:no-cache' );
   header( 'Location:' . home_url( $_SERVER['REQUEST_URI'] ));
   die();
