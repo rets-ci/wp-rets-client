d6d6678064fc2fc5d0ec2aae8f30254b88176ef5
Anton Korotkov <antonkorotkoff@gmail.com>
2015-11-04 19:00:57 +0200

    fix

--- wp-content/themes/wp-madison-child/lib/load.php
+++ wp-content/themes/wp-madison-child/lib/load.php
@@ -27,5 +27,6 @@ add_filter( 'siteorigin_widgets_default_active', function( $widgets ) {
   $widgets['so-recent-posts-widget'] = 1;
   $widgets['so-rating-widget'] = 1;
   $widgets['so-supermap-widget'] = 1;
+  $widgets['so-features-advanced'] = 1;
   return $widgets;
 } );
