ba9b792c9d3a3ad0f8186682e984772c2bdd4bab
Eric Sopp <eric.sopp@usabilitydynamics.com>
2015-11-19 15:43:21 +0000

    Eric Sopp updated Yoast SEO to 3.0.2

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite23ca5b60ac59a8cc7bcf92ac8a8b161::getLoader();
+return ComposerAutoloaderInit443be94e4a7c8827406d2faf32ff8228::getLoader();
--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit886c6483b92ca8f250cd65833859bc65::getLoader();
+return ComposerAutoloaderInit4617381d1fc9a77cfefaeac7fcd01e7b::getLoader();
