05d753c2079d71b784f6a06788acb810b17c9dce
Eric Sopp <eric.sopp@usabilitydynamics.com>
2015-11-19 15:43:30 +0000

    Eric Sopp updated WP-Property to 2.1.3

--- wp-content/plugins/wp-property/lib/classes/class-settings.php
+++ wp-content/plugins/wp-property/lib/classes/class-settings.php
@@ -46,7 +46,8 @@ namespace UsabilityDynamics\WPP {
           'thumbnail_size' => 'tiny_thumb',
           'fancybox_preview' => 'true',
           'display_slideshow' => 'false',
-          'show_children' => 'true'
+          'show_children' => 'true',
+          'pagination_type' => 'slider' // @todo: change to 'numeric' when compatibility will be added to Madison theme. peshkov@UD
         );
 
         $data['configuration']['single_property_view'] = array(
