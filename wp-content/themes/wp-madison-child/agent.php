c76bc666bc63ccf48d775f4ca90b5a5785907d1f
Anton Korotkov <antonkorotkoff@gmail.com>
2015-12-30 18:57:23 +0200

    single agent page wip

--- wp-content/themes/wp-madison-child/agent.php
+++ wp-content/themes/wp-madison-child/agent.php
@@ -6,21 +6,48 @@
  * @author Justin Kopepasah
  * @since 1.0.0
  */
+
+global $wp_query;
 ?>
 
-<?php get_header(); ?>
+<?php get_header('agent'); ?>
 
   <section id="site-primary" class="content-area column col-12-12">
     <main id="site-main" class="content-area-main" role="main">
 
-      <article>
-        <div class="entry-inner">
+      <section class="agent-hero">
+        <div class="background-image"></div>
+        <div class="agent-card container">
+          <div class="row">
+            <div class="col-md-4">
+              <img class="img-responsive agent-photo" src="<?php echo class_agents::get_agent_images($wp_query->agent->ID)[0]['src']; ?>" title="<?php echo $wp_query->agent->display_name; ?>" alt="<?php echo $wp_query->agent->display_name; ?>">
+            </div>
+            <div class="col-md-8">
+              <h2 class="agent-name"><?php echo $wp_query->agent->display_name; ?></h2>
+              <p class="agent-bio">
+                <?php echo get_user_meta( $wp_query->agent->ID, 'widget_bio', 1 ); ?>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="agent-info-line">
+        <div class="container">
+          <div class="col-md-offset-4 col-md-8 phone">
+            <i class="fa fa-phone"></i>
+            <p><?php echo get_user_meta( $wp_query->agent->ID, 'phone_number', 1 ); ?></p>
+          </div>
+        </div>
+      </section>
+
+      <article class="container">
+        <div class="entry-inner ">
           <header class="entry-header">
             <h1 class="entry-title"><?php _e('Browse Homes For Rent'); ?></h1>
           </header>
 
           <div class="entry-content">
-            <?php echo do_shortcode('[property_overview columns=3 per_page=9 sort_by=post_title wpp_agents="33"]'); ?>
+            <?php echo do_shortcode('[property_overview columns=3 per_page=9 sort_by=post_title wpp_agents='.get_query_var('agent').']'); ?>
           </div>
         </div>
       </article>
@@ -28,5 +55,4 @@
     </main>
   </section>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
