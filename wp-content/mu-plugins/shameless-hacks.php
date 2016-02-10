a38599d1804f444a15efcd5cf294b1b6dd7e6f18
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-02-10 21:00:31 +0200

    added a wp-reddoor theme, removed reference to prod DB. changed siteurl/home to use the wpcloud container for develop. removed old themes

--- wp-content/mu-plugins/shameless-hacks.php
+++ wp-content/mu-plugins/shameless-hacks.php
@@ -2,12 +2,3 @@
 /**
  * Shameless Hacks
  */
-
-// Redirect to https mode.c
-if( empty($_SERVER['HTTPS']) ) {
-  header( "HTTP/1.1 301 Moved Permanently" );
-  header( 'Cache-Control:no-cache' );
-  header( 'Location:' . home_url( $_SERVER['REQUEST_URI'] ));
-  die();
-}
-
--- wp-content/themes/madison/js/front-page.js
+++
@@ -1,5 +0,0 @@
-(function( $ ) {
-	if ( $( '#property-featured .property' ).data( 'backstretch' ) ) {
-		$( '#property-featured .property' ).parent().backstretch( $( '.property' ).data( 'backstretch' ) ).find( '.property-image' ).css( 'visibility', 'hidden' );
-	}
-})( jQuery );
