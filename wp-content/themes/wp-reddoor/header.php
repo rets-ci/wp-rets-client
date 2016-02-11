b3ff5fc1fd74b1d08725887ec48d9726742535ad
Kavaribes <kavaribes@gmail.com>
2016-02-11 13:36:21 +0200

    Added core files

---
+++ wp-content/themes/wp-reddoor/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+
+ */
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+        <title><?php bloginfo('name'); ?>  <?php wp_title(); ?></title>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <header>
+		<div class="container-fluid top-line">
+                    <div class="container widthPlansh">
+                        <div class="row">
+                        </div>
+    </header>
