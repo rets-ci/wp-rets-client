e3fea5c3fd2b6d9659e4d99751dd25de44f22481
Kavaribes <kavaribes@gmail.com>
2016-02-12 16:35:55 +0200

    Change theme files
    Added core files(single). Edited style file, footer, archive.

--- wp-content/themes/wp-reddoor/footer.php
+++ wp-content/themes/wp-reddoor/footer.php
@@ -9,25 +9,54 @@
  * @since Twenty Fifteen 1.0
  */
 ?>
-</div><!-- Container -->
 
 <div class="container-fluind footerBg1">
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-lg-4">
-        <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer area 1')) : ?>
-            [ do default stuff if no widgets ]
-        <?php endif; ?>
+            <div class="footerWidgetArea1">
+                <?php if (!dynamic_sidebar('Footer area 1')) : ?>
+                [ do default stuff if no widgets ]
+                <?php endif; ?>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/src/footer-logo.png" alt="" />
+                <p>2530 Meridian Pkwy #300,
+                Durham, NC 27713, USA</p>
+                <a href="#">Contact Us</a>
+            </div>
         </div>
         <div class="col-md-4 col-lg-4">
-        <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer area 2')) : ?>
-            [ do default stuff if no widgets ]
-        <?php endif; ?>
+        <?php //if (!dynamic_sidebar('Footer area 2')) : ?>
+            <!-- [ do default stuff if no widgets ] -->
+        <?php //endif; ?>
+            <div class="footerWidgetArea2">
+                <h3>About Us</h3>
+                <div>
+                    Red Door Company was created in 2006
+                    to make owning investment real estate
+                    easy and profitable. Simply put, we provide
+                    the best property management service in
+                    the business. How do we do it?
+                </div>
+                <span>In Association with</span><br/>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/src/partner1.png" alt="" />
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/src/partner2.png" alt="" />
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/src/partner3.png" alt="" />
+            </div>
         </div>
         <div class="col-md-4 col-lg-4">
-        <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer area 3')) : ?>
-            [ do default stuff if no widgets ]
-        <?php endif; ?>
+        <?php //if (!dynamic_sidebar('Footer area 3')) : ?>
+            <!-- [ do default stuff if no widgets ] -->
+        <?php //endif; ?>
+            <div class="footerWidgetArea3">
+                <h3>Latest Blogs</h3>
+                <ul>
+                    <li>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/src/sliderFootImg.png" alt="" />
+                        <a href="#">How to Make Your Small Space Work for You</a>
+                        <div>Posted on <span>20 Aug 2015</span></div>
+                    </li>
+                </ul>
+            </div>
         </div>
 
 
--- wp-content/themes/wp-reddoor/header.php
+++ wp-content/themes/wp-reddoor/header.php
@@ -4,35 +4,41 @@
 ?><!DOCTYPE html>
 <html>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
-    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <link rel="stylesheet/less" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/static/styles/src/style.less"/>
-    <link href='https://fonts.googleapis.com/css?family=Poppins:400,300,500,700' rel='stylesheet' type='text/css'>
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width">
+  <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+  <link rel="stylesheet/less" type="text/css"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/static/styles/src/style.less"/>
+  <link href='https://fonts.googleapis.com/css?family=Poppins:400,300,500,700' rel='stylesheet' type='text/css'>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
 <div class="container">
-    <div class="row">
-        <header>
-            <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-                <div class='site-logo'>
-                    <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
-                </div>
-            <?php else : ?>
-                <hgroup>
-                    <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
-                    <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
-                </hgroup>
-            <?php endif; ?>
-            <?php wp_nav_menu(array('menu' => 'Header')); ?>
-            <div class="head-popups">
-                <a href="#">Contact</a>
-                <a href="#">Login</a>
-            </div>
-        </header>
-    </div>
+  <div class="row">
+    <header>
+      <?php if (get_theme_mod('themeslug_logo')) : ?>
+        <div class='site-logo'>
+          <a href='<?php echo esc_url(home_url('/')); ?>'
+             title='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>' rel='home'><img
+              src='<?php echo esc_url(get_theme_mod('themeslug_logo')); ?>'
+              alt='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>'></a>
+        </div>
+      <?php else : ?>
+        <hgroup>
+          <h1 class='site-title'><a href='<?php echo esc_url(home_url('/')); ?>'
+                                    title='<?php echo esc_attr(get_bloginfo('name', 'display')); ?>'
+                                    rel='home'><?php bloginfo('name'); ?></a></h1>
+          <h2 class='site-description'><?php bloginfo('description'); ?></h2>
+        </hgroup>
+      <?php endif; ?>
+      <?php wp_nav_menu(array('menu' => 'Header')); ?>
+      <div class="head-popups">
+        <a href="#">Contact</a>
+        <a href="#">Login</a>
+      </div>
+    </header>
+  </div>
+</div>
 
