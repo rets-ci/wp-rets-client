e40024822ebd4b15a6c219a37e14d1611548e0f4
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-02-16 15:35:20 +0200

    added gruntfile and package.json (not tested) to theme

---
+++ wp-content/themes/wp-reddoor/gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "style.css": "static/styles/src/*.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['static/styles/src/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
