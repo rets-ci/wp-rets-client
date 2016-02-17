9a2963da99f81dd3b173410d7552b281e2e63c6b
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-02-17 15:19:58 +0200

    removing plugin (temp) [ci skip]

--- wp-content/plugins/wp-menufication/wp-menufication.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Wordpress Menufication
-Plugin URI: http://www.iveo.se
-Description: Generates a responsive menu from Wordpress menu system or from a custom element. Dependencies: jQuery.
-Version: 1.2
-Author: IVEO
-Author URI: http://www.iveo.se
-License:  © IVEO AB 2013 - All Rights Reserved
