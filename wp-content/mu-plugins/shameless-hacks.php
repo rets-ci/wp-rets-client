4b753ca2020360e158e5540339d9be57f46fecc9
Kavaribes <kavaribes@gmail.com>
2016-03-09 13:25:14 +0200

    Revert "removed SOW_BUNDLE_JS_SUFFIX definition"
    This reverts commit bd9d31b0ba08c4b665a9ba682143a21fe2129a5b.

--- wp-content/mu-plugins/shameless-hacks.php
+++ wp-content/mu-plugins/shameless-hacks.php
@@ -5,7 +5,7 @@
 
 
 // force non-minified version of siteorigin scripts
-// define( 'SOW_BUNDLE_JS_SUFFIX', '' );
+define( 'SOW_BUNDLE_JS_SUFFIX', '' );
 
 if( isset( $_SERVER[ 'HTTP_X_EDGE' ] ) && $_SERVER[ 'HTTP_X_EDGE' ] === '__andy' ) {
   header( 'cache-control:no-cache, private' );
