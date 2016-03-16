3e5887d507012f0307c77f424b424f60c0cd0532
Kavaribes <kavaribes@gmail.com>
2016-03-16 13:50:31 +0200

    Added plugin for carousel [ci skip]

---
+++ wp-content/plugins/ud-property-carousel/ud-property-carousel.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Property carousel
+Description: Gives you possibility to display your properties as a carousel.
+Version: 1.0
+Author: UsabilityDynamics, Inc.
+Author URI: https://www.usabilitydynamics.com
+*/
+?>
+
