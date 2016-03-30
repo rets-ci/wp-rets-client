8716585ac649a8d2c742c359e4350c098adea8f7
Anton Korotkov <antonkorotkoff@gmail.com>
2016-03-30 18:14:42 +0300

    widget views

--- wp-content/themes/wp-reddoor/lib/classes/class-bootstrap.php
+++ wp-content/themes/wp-reddoor/lib/classes/class-bootstrap.php
@@ -143,6 +143,9 @@ namespace UsabilityDynamics\RDC {
         wp_enqueue_style('style', get_stylesheet_directory_uri() . '/static/styles/style.css?nocache='.rand(0,100));
         wp_enqueue_style('style2', 'view-source:https://s3.amazonaws.com/icomoon.io/28703/reddoorcompany/style.css?1gwudq&ver=4.4.2');
         wp_enqueue_style('style1', 'https://s3.amazonaws.com/icomoon.io/28703/wpproperty/style.css?ob605w');
+
+        wp_enqueue_script( 'rdc-custom-validate', 'https://cloud.crm.powerobjects.net/powerWebFormV3/scripts/jquery-1.9.0.validate.min.js', array('jquery') );
+        wp_enqueue_script( 'rdc-custom-ui', 'https://cloud.crm.powerobjects.net/powerWebFormV3/scripts/jquery-ui-1.8.17.custom.min.js', array('jquery') );
       }
 
       /**
