73a7d528c803dba5b940fa6118c5a5502ff944f9
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2016-03-31 08:34:46 +0600

    Revert "Issue #18 cache ignore header fix"
    This reverts commit 51fbdf65c0238bc86ceb4a8160350578cf56e167.

--- lib/shortcodes/supermap.php
+++ lib/shortcodes/supermap.php
@@ -625,9 +625,8 @@ namespace UsabilityDynamics\WPP {
         );
 
         $hashByArguments = md5( serialize( array($query, $args) ) );
-        $headers = getallheaders();
-        $ignore_cache = isset($headers['IGNORE_CACHE'])?$headers['IGNORE_CACHE']:$ignore_cache;
-        if ($ignore_cache == false && ($result = get_transient($hashByArguments))) {
+        $ignore_cache = isset($_SERVER['HTTP_IGNORE_CACHE'])?$_SERVER['HTTP_IGNORE_CACHE']:false;
+        if (($result = get_transient($hashByArguments)) && $ignore_cache == false) {
           if(isset($result['data'])){
             $result['cached'] = true;
             return $result;
