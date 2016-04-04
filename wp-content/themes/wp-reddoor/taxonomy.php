a0d65840a0c9b8b6e52d2508d28b3f0e3d3f8943
Anton Korotkov <antonkorotkoff@gmail.com>
2016-04-04 15:04:03 +0300

    taxonomy archives fixes wip

---
+++ wp-content/themes/wp-reddoor/taxonomy.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Single taxonomy template.
+ * Used for search results.
+ */
+
+global $wp_query;
+
+$tax_args = array();
+foreach( $wp_query->query as $taxonomy => $term ) {
+  $tax_args[] = $taxonomy.'='.$term;
+}
+$tax_args = implode(' ', $tax_args);
+
+get_header(); ?>
+
+  <div class="container-fluid upToHeader">
+    <div class="row">
+      <?php echo do_shortcode("[supermap mode=advanced {$tax_args} bedrooms='1' per_page=5]"); ?>
+    </div><!-- .row -->
+  </div>
+
+<?php get_footer(); ?>
