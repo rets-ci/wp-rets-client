d5ecbc26898688dfa70ecab0ddd4b5d644d31746
Anton Korotkov <antonkorotkoff@gmail.com>
2016-04-18 11:11:39 +0300

    map filter

--- wp-content/themes/wp-reddoor/static/views/supermap/search-form.php
+++ wp-content/themes/wp-reddoor/static/views/supermap/search-form.php
@@ -40,10 +40,10 @@
         <label><?php _e( 'Bedrooms', 'reddoor' ); ?></label>
         <div class="rdc-range-fields">
           <ul>
-            <li><input id="buy_beds_0" class="dropdown-option" name="bool[must][2][range][tax_input.bedrooms][gte]" type="radio" value="0"/><label for="buy_beds_0"><?php _e('No min'); ?></label></li>
+            <li><input id="buy_beds_0" class="dropdown-option styled-checkbox-radio" name="bool[must][2][range][tax_input.bedrooms][gte]" type="radio" value="0"/><label for="buy_beds_0"><?php _e('No min'); ?></label></li>
           <?php foreach( apply_filters('rdc_search_bedrooms_options', array(1,2,3,4,5,6)) as $value ) :
             ?>
-            <li><input id="buy_beds_<?php echo $value; ?>" class="dropdown-option" name="bool[must][2][range][tax_input.bedrooms][gte]" type="radio" value="<?php echo $value; ?>"/><label for="buy_beds_<?php echo $value; ?>"><?php echo $value; ?>+</label></li>
+            <li><input id="buy_beds_<?php echo $value; ?>" class="dropdown-option styled-checkbox-radio" name="bool[must][2][range][tax_input.bedrooms][gte]" type="radio" value="<?php echo $value; ?>"/><label for="buy_beds_<?php echo $value; ?>"><?php echo $value; ?>+</label></li>
           <?php endforeach; ?>
           </ul>
         </div>
@@ -52,20 +52,52 @@
         <label><?php _e( 'Bathrooms', 'reddoor' ); ?></label>
         <div class="rdc-range-fields">
           <ul>
-            <li><input id="buy_beds_0" class="dropdown-option" name="bool[must][3][range][tax_input.bathrooms][gte]" type="radio" value="0"/><label for="buy_beds_0"><?php _e('No min'); ?></label></li>
+            <li><input id="buy_baths_0" class="dropdown-option styled-checkbox-radio" name="bool[must][3][range][tax_input.bathrooms][gte]" type="radio" value="0"/><label for="buy_baths_0"><?php _e('No min'); ?></label></li>
             <?php foreach( apply_filters('rdc_search_bedrooms_options', array(1,2,3,4,5,6)) as $value ) :
               ?>
-              <li><input id="buy_beds_<?php echo $value; ?>" class="dropdown-option" name="bool[must][3][range][tax_input.bathrooms][gte]" type="radio" value="<?php echo $value; ?>"/><label for="buy_beds_<?php echo $value; ?>"><?php echo $value; ?>+</label></li>
+              <li><input id="buy_baths_<?php echo $value; ?>" class="dropdown-option styled-checkbox-radio" name="bool[must][3][range][tax_input.bathrooms][gte]" type="radio" value="<?php echo $value; ?>"/><label for="buy_baths_<?php echo $value; ?>"><?php echo $value; ?>+</label></li>
             <?php endforeach; ?>
           </ul>
         </div>
       </div>
       <div class="col-md-4">
+
         <label><?php _e( 'Price', 'reddoor' ); ?></label>
         <div class="rdc-range-fields">
           <input placeholder="<?php _e('Min'); ?>" type="text" name="bool[must][4][range][tax_input.price][gte]" />
           <input placeholder="<?php _e('Max'); ?>" type="text" name="bool[must][4][range][tax_input.price][lte]" />
         </div>
+
+        <label><?php _e( 'Home Type', 'reddoor' ); ?></label>
+        <div class="rdc-range-fields">
+          <ul>
+            <li>
+              <label>
+                <input id="property_type_condo" class="styled-checkbox-radio" type="checkbox" value="condo" checked name="bool[must][6][terms][meta_input.property_type][]" />
+                <label for="property_type_condo"><?php _e('Condo', 'reddor'); ?></label>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input id="property_type_townhouse" class="styled-checkbox-radio" type="checkbox" value="townhouse" checked name="bool[must][6][terms][meta_input.property_type][]" />
+                <label for="property_type_townhouse"><?php _e('Townhouse', 'reddor'); ?></label>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input id="property_type_house" class="styled-checkbox-radio" type="checkbox" value="house" checked name="bool[must][6][terms][meta_input.property_type][]" />
+                <label for="property_type_house"><?php _e('House', 'reddor'); ?></label>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input id="property_type_manufactured" class="styled-checkbox-radio" type="checkbox" value="manufactured" checked name="bool[must][6][terms][meta_input.property_type][]" />
+                <label for="property_type_manufactured"><?php _e('Manufactured', 'reddor'); ?></label>
+              </label>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
 
@@ -78,44 +110,14 @@
           <ul>
             <li>
               <label>
-                <input type="checkbox" value="Sale" name="bool[must][5][terms][tax_input.sale_type][]" />
-                <?php _e('Sale', 'reddor'); ?>
+                <input id="sale_type_sale" class="styled-checkbox-radio" type="checkbox" value="Sale" name="bool[must][5][terms][tax_input.sale_type][]" />
+                <label for="sale_type_sale"><?php _e('Sale', 'reddor'); ?></label>
               </label>
             </li>
             <li>
               <label>
-                <input type="checkbox" value="Rent" name="bool[must][5][terms][tax_input.sale_type][]" />
-                <?php _e('Rent', 'reddor'); ?>
-              </label>
-            </li>
-          </ul>
-        </div>
-
-        <label><?php _e( 'Home Type', 'reddoor' ); ?></label>
-        <div class="rdc-range-fields">
-          <ul>
-            <li>
-              <label>
-                <input type="checkbox" value="condo" checked name="bool[must][6][terms][meta_input.property_type][]" />
-                <?php _e('Condo', 'reddor'); ?>
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" value="townhouse" checked name="bool[must][6][terms][meta_input.property_type][]" />
-                <?php _e('Townhouse', 'reddor'); ?>
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" value="house" checked name="bool[must][6][terms][meta_input.property_type][]" />
-                <?php _e('House', 'reddor'); ?>
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" value="manufactured" checked name="bool[must][6][terms][meta_input.property_type][]" />
-                <?php _e('Manufactured', 'reddor'); ?>
+                <input id="sale_type_rent" class="styled-checkbox-radio" type="checkbox" value="Rent" name="bool[must][5][terms][tax_input.sale_type][]" />
+                <label for="sale_type_rent"><?php _e('Rent', 'reddor'); ?></label>
               </label>
             </li>
           </ul>
