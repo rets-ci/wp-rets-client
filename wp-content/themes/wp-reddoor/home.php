20914a8a40f951c08acf82129f2784159db198bf
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-04-21 11:48:46 +0300

    added support for featured image to work on blog home page. closes https://github.com/UsabilityDynamics/www.reddoorcompany.com/issues/150

--- wp-content/themes/wp-reddoor/home.php
+++ wp-content/themes/wp-reddoor/home.php
@@ -8,6 +8,7 @@
  */
 
 
+// Detect which post/page is used as the "page for posts" to get its meta later. - potanin@UD
 $_page_for_posts = get_option('page_for_posts');
 
 get_header();
@@ -15,8 +16,8 @@ get_header();
 ?>
 <div class="container-fluid ftrdImgGoTop">
   <section class="archiveImageHeader" style="<?php if( has_post_thumbnail( $_page_for_posts ) ) { ?>background-image: url('<?php echo get_the_post_thumbnail_url( $_page_for_posts, 'full' ); ?>'); "<?php } ?>>
-    <h1><?php echo get_post_meta($_page_for_posts , 'rdcPageTitle', 1); ?></h1>
-    <h3><?php echo get_post_meta($_page_for_posts , 'rdcPageSubtitle', 1); ?></h3>
+    <h1><?php echo $_page_for_posts ? get_post_meta($_page_for_posts , 'rdcPageTitle', 1) : ''; ?></h1>
+    <h3><?php echo $_page_for_posts ? get_post_meta($_page_for_posts , 'rdcPageSubtitle', 1) : ''; ?></h3>
     <div class="hero-overlay"></div>
   </section>
 </div>
