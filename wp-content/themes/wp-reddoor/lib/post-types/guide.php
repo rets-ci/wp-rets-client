710129e4787b0d05a4ef371442b8dc7c70e1f7b3
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-04-21 15:04:34 +0300

    added thumbnail to guides admin for preview

--- wp-content/themes/wp-reddoor/lib/post-types/guide.php
+++ wp-content/themes/wp-reddoor/lib/post-types/guide.php
@@ -100,6 +100,25 @@ function register_guide_category_taxonomy() {
 
 add_filter('post_link', 'guide_category_permalink', 1, 3);
 add_filter('post_type_link', 'guide_category_permalink', 1, 3);
+add_action( 'manage_rdc_guide_posts_custom_column' , 'guide_custom_columns', 10, 2 );
+add_filter( 'manage_rdc_guide_posts_columns', 'set_custom_edit_book_columns' );
+
+function set_custom_edit_book_columns($columns) {
+  $columns['thumbnail'] = __( 'Thumbnail', 'your_text_domain' );
+
+  return $columns;
+}
+
+
+
+function guide_custom_columns( $column, $post_id ) {
+  switch ( $column ) {
+    case 'thumbnail':
+      echo "<img src='" . get_the_post_thumbnail_url( $post_id, 'thumbnail' ) . "' width=100 />";
+    break;
+
+  }
+}
 
 function guide_category_permalink($permalink, $post_id, $leavename) {
   //con %brand% catturo il rewrite del Custom Post Type
