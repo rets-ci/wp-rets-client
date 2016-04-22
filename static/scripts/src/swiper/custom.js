8f3b2d256713dfd02f275e9df1e1b9fb6af51d42
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2016-04-22 14:39:31 +0300

    Fixed javascript error for slider_type=12grid

--- static/scripts/src/swiper/custom.js
+++ static/scripts/src/swiper/custom.js
@@ -42,6 +42,10 @@ s.setSlideSize_12mosaic = function(slide, s){
 }
 
 s.setSlideSize_12grid = function(slide, s){
+    if( typeof slide == 'undefined' ) {
+        return 0;
+    }
+    
     var width, height;
     var top = 0, left = 0;
     var slideWidth, slideHeight, slideRatio;
@@ -49,6 +53,10 @@ s.setSlideSize_12grid = function(slide, s){
     var slide = jQuery(slide);
     var img = slide.find('img');
     var maxHeight = (s.container.height() - s.params.spaceBetween) / s.params.slidesPerColumn;
+
+    if( typeof img[0] == 'undefined' ) {
+        return 0;
+    }
 
     var attrWidth  = parseInt(img.attr('width'));
     var attrHeight  = parseInt(img.attr('height'));
