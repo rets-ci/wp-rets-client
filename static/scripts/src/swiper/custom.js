5c39168e96a9f25b23120cb33d80139d52dbf0f6
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2016-04-22 14:41:21 +0300

    Fixed javascript error for slider_type=12mosaic

--- static/scripts/src/swiper/custom.js
+++ static/scripts/src/swiper/custom.js
@@ -16,10 +16,18 @@ s.isLightbox = function(){
 }
 
 s.setSlideSize_12mosaic = function(slide, s){
+    if( typeof slide == 'undefined' ) {
+        return 0;
+    }
+    
     var width, height;
     var slide = jQuery(slide);
     var img = slide.find('img');
     var maxHeight = s.container.height() / s.params.slidesPerColumn;
+
+    if( typeof img[0] == 'undefined' ) {
+        return 0;
+    }
 
     var attrWidth  = parseInt(img.attr('width'));
     var attrHeight  = parseInt(img.attr('height'));
