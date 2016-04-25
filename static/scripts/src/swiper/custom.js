a26ba8a3f375fed1978c18f9b06c2146b9c2fdc8
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-04-25 15:07:57 +0300

    Squashed 'wp-content/plugins/wp-property-responsive-slideshow/' changes from f606e19..f930ca3
    f930ca3 Fixed javascript error for 12grid and 12mosaic
    5c39168 Fixed javascript error for slider_type=12mosaic
    8f3b2d2 Fixed javascript error for slider_type=12grid
    6745bd8 Merge remote-tracking branch 'origin/latest' into latest
    18236c7 Issue #41
    5ab65b7 Update readme.md
    
    git-subtree-dir: wp-content/plugins/wp-property-responsive-slideshow
    git-subtree-split: f930ca315a3edb88134ee380fb0bea75ab7df26c

--- static/scripts/src/swiper/custom.js
+++ static/scripts/src/swiper/custom.js
@@ -16,10 +16,18 @@ s.isLightbox = function(){
 }
 
 s.setSlideSize_12mosaic = function(slide, s){
+    if( typeof slide == 'undefined' ) {
+        return 0;
+    }
+    
     var width, height;
     var slide = jQuery(slide);
     var img = slide.find('img');
     var maxHeight = s.container.height() / s.params.slidesPerColumn;
+
+    if( typeof img[0] == 'undefined' ) {
+        return 0;
+    }
 
     var attrWidth  = parseInt(img.attr('width'));
     var attrHeight  = parseInt(img.attr('height'));
@@ -42,6 +50,10 @@ s.setSlideSize_12mosaic = function(slide, s){
 }
 
 s.setSlideSize_12grid = function(slide, s){
+    if( typeof slide == 'undefined' ) {
+        return 0;
+    }
+    
     var width, height;
     var top = 0, left = 0;
     var slideWidth, slideHeight, slideRatio;
@@ -49,6 +61,10 @@ s.setSlideSize_12grid = function(slide, s){
     var slide = jQuery(slide);
     var img = slide.find('img');
     var maxHeight = (s.container.height() - s.params.spaceBetween) / s.params.slidesPerColumn;
+
+    if( typeof img[0] == 'undefined' ) {
+        return 0;
+    }
 
     var attrWidth  = parseInt(img.attr('width'));
     var attrHeight  = parseInt(img.attr('height'));
