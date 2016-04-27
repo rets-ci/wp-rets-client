a0a73a79f8b0abf8f74f3c044483cdaba6958be5
Anton Korotkov <antonkorotkoff@gmail.com>
2016-04-27 19:03:36 +0300

    fix to /blog/ on taxonomies https://github.com/UsabilityDynamics/www.reddoorcompany.com/issues/294

--- wp-content/themes/wp-reddoor/lib/classes/class-bootstrap.php
+++ wp-content/themes/wp-reddoor/lib/classes/class-bootstrap.php
@@ -80,6 +80,7 @@ namespace UsabilityDynamics\RDC {
         add_filter( 'wpp:supermap:query_defaults', array( $this, 'alter_supermap_query' ), 10, 2 );
 
         add_filter( 'wpp_post_type', array( $this, 'wpp_post_type_alter' ) );
+        add_filter( 'wpp::register_taxonomy', array( $this, 'wpp_post_type_alter' ) );
 
         // facebook/twitter/google shortcode
         add_shortcode( 'share_this_article', array( 'UsabilityDynamics\RDC\Shortcodes', 'share_this_article' ) );
