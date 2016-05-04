d5ecf2283b9770a5b80010787d779292565c3809
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-05-04 19:44:16 +0300

    add support for showContactPopup for buttons using data-action argument. related to https://github.com/UsabilityDynamics/www.reddoorcompany.com/issues/146

--- wp-content/themes/wp-reddoor/static/scripts/src/popups.js
+++ wp-content/themes/wp-reddoor/static/scripts/src/popups.js
@@ -2,13 +2,7 @@ jQuery(function () {
 
   var popUpWindow = jQuery('span.exitPopup').parent().parent();
 
-  jQuery('.showContactPopup > a, button.showContactPopup, a.showContactPopup').on('click', function showContactPopup() {
-    var that = this;
-    jQuery('div.popup').fadeOut(300);
-    jQuery('div.popup.'+jQuery(that).attr("rel")).fadeIn(200);
-    jQuery('html').css('overflow-y','hidden');
-    return false;
-  });
+  jQuery('.showContactPopup > a, button.showContactPopup, a.showContactPopup').on('click', showContactPopup );
 
   jQuery('span.exitPopup').on('click', function () {
     jQuery(this).parent().parent().fadeOut(100);
@@ -31,4 +25,29 @@ jQuery(function () {
   jQuery('.popup .hidden-phone').on('click', function(){
     jQuery( this ).val(jQuery( this ).data('phone'));
   });
+
+  /**
+   * Show Popup Box
+   *
+   * @param event
+   * @returns {boolean}
+   */
+  function showContactPopup( event ) {
+
+    var that = this;
+
+    // prevent actual click from going through
+    event.preventDefault();
+
+    // determine type of action we're making with the click
+    var _type = jQuery(that).attr("rel") || jQuery(that).attr("data-action");
+
+    jQuery('div.popup').fadeOut(300);
+    jQuery('div.popup.'+ _type ).fadeIn(200);
+    jQuery('html').css('overflow-y','hidden');
+
+    return false;
+
+  }
+
 });
