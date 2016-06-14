dbc30c82a5a95758e60bd8e901fc65e139134662
Mehul <mehul.kaklotar@gmail.com>
2016-06-14 14:15:14 +0530

    sort smart table fix

--- wp-content/plugins/wp-property-supermap/static/scripts/advanced/supermap.js
+++ wp-content/plugins/wp-property-supermap/static/scripts/advanced/supermap.js
@@ -881,7 +881,7 @@
           jQuery('.rdc-longitude-lte').val('');
         };
 
-        jQuery('table.sm-properties-list,ul.sm-columns-sorter').bind("DOMSubtreeModified", function(){
+        $document.bind("st_sort_done",function(){
           if( $scope.propertiesTableCollection.length > 0 ) {
             $scope.selectRow($scope.propertiesTableCollection[0]);
           }
