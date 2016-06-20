263ca119354abbd903a59fcaef1ab3ac70060836
Kavaribes <kavaribes@gmail.com>
2016-06-20 18:21:20 +0300

    Fixed styles for Tabbed Widget on Iphone5. Added Masthead Widget files.

---
+++ wp-content/themes/wp-reddoor/lib/widgets/rdc-masthead/masthead.php
@@ -0,0 +1,256 @@
+<?php
+/*
+Widget Name: Hero Image
+Description: A big hero image with a few settings to make it your own.
+Author: SiteOrigin
+Author URI: https://siteorigin.com
+*/
+
+if( !class_exists( 'SiteOrigin_Widget_Base_Slider' ) ) include_once get_stylesheet_directory().'/lib/widgets/rdc-masthead/base/base-slider.class.php';
+
+class Masthead_Widget extends SiteOrigin_Widget_Base_Slider {
+
+	protected $buttons = array();
+
+	function __construct() {
+		parent::__construct(
+			'rdc-masthead',
+			__('RDC Masthead', 'so-widgets-bundle'),
+			array(
+				'description' => __('RDC Masthead widget', 'so-widgets-bundle'),
+				'help' => 'https://siteorigin.com/widgets-bundle/hero-image-widget/',
+				'panels_title' => false,
+			),
+			array( ),
+			array(
+				'title' => array(
+					'type' => 'text',
+					'label' => __( 'Title', 'so-widgets-bundle' ),
+				),
+
+				'frames' => array(
+					'type' => 'repeater',
+					'label' => __('Tabbed frames', 'so-widgets-bundle'),
+					'item_name' => __('Frame', 'so-widgets-bundle'),
+					'item_label' => array(
+						'selector' => "[id*='frames-title']",
+						'update_event' => 'change',
+						'value_method' => 'val'
+					),
+
+					'fields' => array(
+
+						'image' => array(
+							'type' => 'media',
+							'label' => __( 'Image', 'so-widgets-bundle' ),
+							'library' => 'image',
+							'fallback' => true,
+						),
+
+						'bgcolor' => array(
+							'type' => 'color',
+							'label' => __( 'Background Color', 'so-widgets-bundle' ),
+						),
+
+						'content_title' => array(
+							'type' => 'text',
+							'label' => __( 'Content title', 'so-widgets-bundle' ),
+						),
+
+						'content' => array(
+							'type' => 'tinymce',
+							'label' => __( 'Content', 'so-widgets-bundle' ),
+						),
+
+						'svg' => array(
+							'type' => 'icon',
+							'label' => __( 'Navigation icon', 'so-widgets-bundle' ),
+						),
+
+						'feature_point' => array(
+							'type' => 'section',
+							'label' => __('Feature point', 'so-widgets-bundle'),
+							'fields' => array(
+								'icon_point' => array(
+									'type' => 'icon',
+									'label' => __( 'Select icon', 'so-widgets-bundle' ),
+								),
+
+								'feature_title' => array(
+									'type' => 'text',
+									'label' => __( 'Feature title', 'so-widgets-bundle' ),
+								),
+
+								'feature_content' => array(
+									'type' => 'tinymce',
+									'label' => __( 'Feature content', 'so-widgets-bundle' ),
+								),
+							),
+						),
+
+						'feature_point2' => array(
+							'type' => 'section',
+							'label' => __('Feature point', 'so-widgets-bundle'),
+							'fields' => array(
+								'icon_point2' => array(
+									'type' => 'icon',
+									'label' => __( 'Select icon', 'so-widgets-bundle' ),
+								),
+
+								'feature_title2' => array(
+									'type' => 'text',
+									'label' => __( 'Feature title', 'so-widgets-bundle' ),
+								),
+
+								'feature_content2' => array(
+									'type' => 'tinymce',
+									'label' => __( 'Feature content', 'so-widgets-bundle' ),
+								),
+							),
+						),
+						'feature_point3' => array(
+							'type' => 'section',
+							'label' => __('Feature point', 'so-widgets-bundle'),
+							'fields' => array(
+								'icon_point3' => array(
+									'type' => 'icon',
+									'label' => __( 'Select icon', 'so-widgets-bundle' ),
+								),
+
+								'feature_title3' => array(
+									'type' => 'text',
+									'label' => __( 'Feature title', 'so-widgets-bundle' ),
+								),
+
+								'feature_content3' => array(
+									'type' => 'tinymce',
+									'label' => __( 'Feature content', 'so-widgets-bundle' ),
+								),
+							),
+						),
+
+					),
+				),
+
+			)
+		);
+
+	}
+
+	/**
+	 * Get everything necessary for the background image.
+	 *
+	 * @param $i
+	 * @param $frame
+	 *
+	 * @return array
+	 */
+	function get_frame_background( $i, $frame ){
+		$background_image = siteorigin_widgets_get_attachment_image_src(
+			isset( $frame['background'] ) ? $frame['background']['image'] : null,
+			'full',
+			!empty( $frame['background']['image_fallback'] ) ? $frame['background']['image_fallback'] : ''
+		);
+
+		return array(
+			'color' => !empty( $frame['background']['color'] ) ? $frame['background']['color'] : false,
+			'image' => !empty( $background_image ) ? $background_image[0] : false,
+			'image-sizing' => 'cover',
+			'url' => !empty( $frame['background']['url'] ) ? $frame['background']['url'] : false,
+			'new_window' => !empty( $frame['background']['new_window'] ),
+			'video-sizing' => 'background',
+			'opacity' => isset( $frame['background'] ) ? ( intval($frame['background']['opacity'])/100 ) : 1,
+		);
+	}
+
+	/**
+	 * Render the actual content of the frame
+	 *
+	 * @param $i
+	 * @param $frame
+	 */
+	function render_frame_contents($i, $frame) {
+		?>
+		<div class="sow-slider-image-container">
+			<div class="sow-slider-image-wrapper">
+				<?php	$dataImage = wp_get_attachment_image_url($frame['image'], 'full');	?>
+				<div class="tabbedWidgetImageArea" style="background-image: url('<?php echo $dataImage ?>');"></div>
+				<div class="tabbedWidgetContent" style="<?php echo (!empty( $frame['bgcolor'] ) ? "background-color:" . $frame['bgcolor'] : ""); ?>">
+				<h3><?php echo $frame['content_title'] ?></h3>
+					<p><?php echo $frame['content'] ?></p>
+					<div class="featurePoint">
+						<?php echo siteorigin_widget_get_icon($frame['feature_point']['icon_point']); ?>
+						<div <?php if(empty(siteorigin_widget_get_icon($frame['feature_point']['icon_point']))){echo 'style="width: 100%"'; } ?>>
+							<h4><?php echo $frame['feature_point']['feature_title'] ?></h4>
+							<?php echo $frame['feature_point']['feature_content'] ?>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<?php if($frame['feature_point2']['icon_point2'] || $frame['feature_point2']['feature_title2'] || $frame['feature_point2']['feature_content2'] ){ ?>
+					<div class="featurePoint">
+						<?php echo siteorigin_widget_get_icon($frame['feature_point2']['icon_point2']); ?>
+						<div <?php if(empty(siteorigin_widget_get_icon($frame['feature_point2']['icon_point2']))){echo 'style="width: 100%"'; } ?>>
+							<h4><?php echo $frame['feature_point2']['feature_title2'] ?></h4>
+							<?php echo $frame['feature_point2']['feature_content2'] ?>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<?php } ?>
+					<?php if($frame['feature_point3']['icon_point3'] || $frame['feature_point3']['feature_title3'] || $frame['feature_point3']['feature_content3'] ){ ?>
+						<div class="featurePoint">
+							<?php echo siteorigin_widget_get_icon($frame['feature_point3']['icon_point3']); ?>
+							<div <?php if(empty(siteorigin_widget_get_icon($frame['feature_point3']['icon_point3']))){echo 'style="width: 100%"'; } ?>>
+								<h4><?php echo $frame['feature_point3']['feature_title3'] ?></h4>
+								<?php echo $frame['feature_point3']['feature_content3'] ?>
+							</div>
+							<div class="clear"></div>
+						</div>
+					<?php } ?>
+				</div>
+
+				<div class="clear"></div>
+			</div>
+		</div>
+
+		<?php
+	}
+
+	/**
+	 * Process the content. Most importantly add the buttons by replacing [buttons] in the content
+	 *
+	 * @param $content
+	 * @param $frame
+	 *
+	 * @return string
+	 */
+	function process_content( $content, $frame ) {
+		ob_start();
+
+		// Add in the button code
+		$san_content = wp_kses_post($content);
+		$content = preg_replace('/(?:<(?:p|h\d|em|strong|li|blockquote) *([^>]*)> *)?\[ *buttons *\](:? *<\/(?:p|h\d|em|strong|li|blockquote)>)?/i', '<div class="sow-hero-buttons" $1>' . $button_code . '</div>', $san_content );
+		return $content;
+	}
+
+	/**
+	 * The less variables to control the design of the slider
+	 *
+	 * @param $instance
+	 *
+	 * @return array
+	 */
+
+
+	function add_default_measurement_unit($val) {
+		if (!empty($val)) {
+			if (!preg_match('/\d+([a-zA-Z%]+)/', $val)) {
+				$val .= 'px';
+			}
+		}
+		return $val;
+	}
+
+
+}
+
+siteorigin_widget_register('rdc-masthead', __FILE__, 'RDC Masthead');
---
+++ wp-content/themes/wp-reddoor/masthead-template.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Template Name: masthead-template
+*/
+
+get_header(); ?>
+<div class="container-fluid full-width-totop">
+    <div class="row site-content">
+        <?php while (have_posts()) : the_post(); ?>
+            <?php
+            the_content();
+            ?>
+        <?php endwhile; // end of the loop. ?>
+    </div><!-- .row -->
+</div>
+
+<?php get_footer(); ?>
