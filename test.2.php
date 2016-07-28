12e928a61aa86a5f701cd24904a574dc73d90ff9
andypotanin <andy.potanin@usabilitydynamics.com>
2016-07-28 03:35:42 +0200

    cleaned up shameles hacks. added cloudfront mu plugin (not setup yet)

---
+++ test.2.php
@@ -0,0 +1,5 @@
+<?php
+die(json_encode(array(
+	"time" => time(),
+	"query" => $_REQUEST
+)));
---
+++ wp-content/mu-plugins/cloudfront.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: CloudFront Handler
+ * Plugin URI: http://usabilitydynamics.com/plugins/
+ * Description: Handles CloudFront domains.
+ * Author: Usability Dynamics, Inc.
+ * Version: 0.1.0
+ * Author URI: http://usabilitydynamics.com
+ *
+ */
