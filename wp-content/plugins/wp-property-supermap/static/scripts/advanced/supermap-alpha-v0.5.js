0141b411d9af78e8b0856fc7ef381864f7598de7
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-08-19 12:13:11 -0400

    adding missing function

--- wp-content/plugins/wp-property-supermap/static/scripts/advanced/supermap-alpha-v0.5.js
+++ wp-content/plugins/wp-property-supermap/static/scripts/advanced/supermap-alpha-v0.5.js
@@ -1934,6 +1934,24 @@
   }
 
   /**
+   * Convert URL Query to Object
+   *
+   * @param qstr
+   * @returns {{}}
+   */
+  function parse_query_string(qstr) {
+    var query = {};
+    var a = qstr.substr(1).split('&');
+    for (var i = 0; i < a.length; i++) {
+      var b = a[i].split('=');
+      query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+
+    return query;
+  }
+
+  
+  /**
    *
    * @param str
    * @param array
