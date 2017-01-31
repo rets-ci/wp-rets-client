0b594a04657dde33dda433409c9962aca7c68856
Andy Potanin <andy@udx.io>
2017-01-31 18:14:22 -0500

    tweaking request url

--- views/rets-mapper-ui.php
+++ views/rets-mapper-ui.php
@@ -1,11 +1,27 @@
 <?php
 
-$api_url = 'https://rets-ci-node-client-service-latest.c.rabbit.ci/' . get_option('ud_site_id') . '/analysis?token=' . get_option('ud_site_secret_token');
+$api_url = 'https://rets-ci-api-rets-ci-v0-5-0.c.rabbit.ci/v1/site/' . get_option('ud_site_id') . '/analysis?token=' . get_option('ud_site_secret_token');
 
 $_analysis = json_decode( wp_remote_retrieve_body( wp_remote_get($api_url) ) );
-
 wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.5/js/jquery.tablesorter.min.js' );
+
 ?>
+
+<style>
+
+  .wp-rets-mapper-group[data-rets-mapper-group="post"],
+  .wp-rets-mapper-group[data-rets-mapper-group="_standard"],
+  .wp-rets-mapper-group[data-rets-mapper-group="_categorical"],
+  .wp-rets-mapper-group[data-rets-mapper-group="_system"] {
+    display: none;
+  }
+
+  #rets-mapper-table.widefat * {
+    word-wrap: normal;
+  }
+</style>
+
+
 <div class="wrap">
   <h3>RETS.ci Field Mapper</h3>
 <p><?php echo $_analysis->message; ?>.</p>
@@ -25,7 +41,7 @@ wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery
   </thead>
   <tbody>
 <?php  foreach( $_analysis->data as $_field ) { ?>
-  <tr>
+  <tr class="wp-rets-mapper-group wp-rets-mapper-group-<?php echo $_field->group ? $_field->group : 'post'; ?>" data-rets-mapper-group="<?php echo $_field->group ? $_field->group : 'post'; ?>">
 
     <th class="hidden"><?php echo $_field->_id; ?></th>
     <th><?php echo $_field->key; ?></th>
@@ -61,8 +77,3 @@ echo( '<pre class="hidden">' . print_r( $_analysis, true ) . '</pre>' ); ?>
 
 
 </script>
-<style>
-  #rets-mapper-table.widefat * {
-    word-wrap: normal;
-  }
-</style>
--- wp-rets-mapper.php
+++ wp-rets-mapper.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.usabilitydynamics.com/product/wp-crm/
  * Description: Map fields
  * Author: Usability Dynamics, Inc.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Requires at least: 4.0
  * Tested up to: 4.7
  * https://rets-ci-api-rets-ci-v0-5-0.c.rabbit.ci/v1/site/616bf200-b814-4a8b-816e-a4405061e3b8/analysis?token=40f95eaadca4472e7213f1f7d6ead1a7
