68362aca2fece119de37c2613ad9627a69bd7900
Andy Potanin <andy@udx.io>
2017-01-31 06:56:37 -0500

    switching wp-property to subtree

--- wp-content/plugins/wp-property/uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Not like register_uninstall_hook(), you do NOT have to use a static function.
-
-if( function_exists( 'wpp_fs' ) ) {
-  wpp_fs()->add_action('after_uninstall', 'wpp_fs_uninstall_cleanup');
-}
