85f413b2d4863ea45a24c86a095ba61684e61a76
Andy Potanin <andy@udx.io>
2017-01-31 10:13:06 -0500

    port from rdc

---
+++ wp-rets-mapper.php
@@ -0,0 +1,18 @@
+<?php
+ * Plugin Name: rets.ci mapper
+ * Plugin URI: https://www.usabilitydynamics.com/product/wp-crm/
+ * Description: Map fields
+ * Author: Usability Dynamics, Inc.
+ * Version: 0.1.0
+ * Requires at least: 4.0
+ * Tested up to: 4.7
+ * https://rets-ci-api-rets-ci-v0-5-0.c.rabbit.ci/v1/site/616bf200-b814-4a8b-816e-a4405061e3b8/analysis?token=40f95eaadca4472e7213f1f7d6ead1a7
+ *
+ */
+add_action('admin_menu', function() {
+
+  add_dashboard_page('Mapper', 'Mapper', 'read', 'rets-mapper', function() {
+    include_once( __DIR__ . '/views/rets-mapper-ui.php' );
+
+  } );
+});
