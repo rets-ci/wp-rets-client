5e8f721633700bc4a09bf8c70c9b8a8215a08699
Andy Potanin <andy@udx.io>
2017-02-01 23:06:47 -0500

    Added option to hide rows from visibility.

--- views/rets-mapper-ui.php
+++ views/rets-mapper-ui.php
@@ -1,8 +1,14 @@
 <?php
 
-$api_url = 'https://rets-ci-api-rets-ci-v0-5-0.c.rabbit.ci/v1/site/' . get_option('ud_site_id') . '/analysis?token=' . get_option('ud_site_secret_token');
+if( get_transient( 'wp-rets-mapper-data' ) ) {
+  $_analysis = get_transient( 'wp-rets-mapper-data' );
+} else {
 
-$_analysis = json_decode( wp_remote_retrieve_body( wp_remote_get($api_url) ) );
+  $api_url = 'https://rets-ci-api-rets-ci-v0-5-0.c.rabbit.ci/v1/site/' . get_option( 'ud_site_id' ) . '/analysis?token=' . get_option( 'ud_site_secret_token' );
+  $_analysis = json_decode( wp_remote_retrieve_body( wp_remote_get( $api_url ) ) );
+  set_transient( 'wp-rets-mapper-data', $_analysis, 90 );
+}
+
 wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.5/js/jquery.tablesorter.min.js' );
 
 ?>
@@ -29,21 +35,22 @@ wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery
 <table id="rets-mapper-table" class="widefat fixed tablesorter">
   <thead>
   <tr>
-    <th scope="col"  class="hidden">ID</th>
+    <th scope="col" class="hidden">ID</th>
     <th scope="col" class="sortable"><a href="#"><span>Slug</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable"><a href="#"><span>Count</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable hidden"><a href="#"><span>Type</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable"><a href="#"><span>Rate</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable"><a href="#"><span>Group</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable"><a href="#"><span>Values</span><span class="sorting-indicator"></span></a></th>
-    <th scope="col"  class="sortable"><a href="#"><span>Alias</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable"><a href="#"><span>Count</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable hidden"><a href="#"><span>Type</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable"><a href="#"><span>Rate</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable"><a href="#"><span>Group</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable"><a href="#"><span>Values</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class="sortable"><a href="#"><span>Alias</span><span class="sorting-indicator"></span></a></th>
+    <th scope="col" class=""><a href="#"><span>Action</span><span class="sorting-indicator"></span></a></th>
   </tr>
   </thead>
   <tbody>
   <?php $c = 0; ?>
-<?php foreach( $_analysis->data as $_field ) { ?>
-  <?php $c++; ?>
-  <tr class="wp-rets-mapper-group wp-rets-mapper-group-<?php echo $_field->group ? $_field->group : 'post'; ?>" data-rets-mapper-group="<?php echo $_field->group ? $_field->group : 'post'; ?>">
+  <?php foreach( $_analysis->data as $_field ) { ?>
+    <?php $c++; ?>
+    <tr class="wp-rets-mapper-group wp-rets-mapper-group-<?php echo $_field->group ? $_field->group : 'post'; ?>" data-rets-mapper-id="<?php echo $_field->_id; ?>" data-rets-mapper-group="<?php echo $_field->group ? $_field->group : 'post'; ?>">
     
     <th class="hidden"><?php echo $_field->_id; ?></th>
     <th><?php echo $_field->key; ?></th>
@@ -53,14 +60,14 @@ wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery
     <td><?php echo $_field->group; ?></td>
     <td class="mapper-show-info" data-info-block="block-<?php echo $c; ?>" style="color:blue;font-weight: bold;cursor:pointer">
       <?php
-      if(isset( $_field->values  )){
+      if( isset( $_field->values ) ) {
         echo count( $_field->values );
         $i = 0; ?>
         <div class="hidden block-<?php echo $c; ?>" style="padding: 2px;font-style: italic">
           <?php
-          foreach($_field->values as $value){
+          foreach( $_field->values as $value ) {
             $i++;
-            if($i < 5){
+            if( $i < 10 ) {
               echo $value->value . '<br/>';
             }
           }
@@ -69,31 +76,68 @@ wp_enqueue_script( 'tablesorter', 'https://cdnjs.cloudflare.com/ajax/libs/jquery
       <?php }; ?>
     </td>
     <td><input type="text" value=""></td>
+    <td><a class="wp-rets-mapper-action" data-action="hide">Hide</a></td>
 
   </tr>
 
-<?php } ?>
+  <?php } ?>
   </tbody>
 
 </table>
 
-<?php
+  <?php
 
-echo( '<pre class="hidden">' . print_r( $_analysis, true ) . '</pre>' ); ?>
+  echo( '<pre class="hidden">' . print_r( $_analysis, true ) . '</pre>' ); ?>
   </div>
 
 <script>
-  jQuery(document).ready(function()
-    {
-      jQuery("#rets-mapper-table").tablesorter({
+  jQuery( document ).ready( function () {
+      jQuery( ".wp-rets-mapper-action" ).click( function () {
+
+        var _id = jQuery( this ).closest( '.wp-rets-mapper-group' ).data( 'rets-mapper-id' );
+
+        jQuery( this ).closest( '.wp-rets-mapper-group' ).hide();
+
+        var _hidden;
+
+        try {
+
+          _hidden = JSON.parse( localStorage.getItem( 'wp-rets-hidden-fields' ) ) || [];
+
+        } catch( error ) {
+          _hidden = [];
+        }
+
+        if( 'string' === typeof _hidden ) {
+          _hidden = [ _hidden ];
+        }
+
+        _hidden.push( _id );
+
+        localStorage.setItem( 'wp-rets-hidden-fields', JSON.stringify( _hidden ) );
+
+      } );
+
+    // Hide hidden fields
+    if( localStorage.getItem( 'wp-rets-hidden-fields' ) ) {
+      var _hidden = JSON.parse( localStorage.getItem( 'wp-rets-hidden-fields' ) );
+
+      _hidden.forEach(function( hiddenItem ) {
+        jQuery( '.wp-rets-mapper-group[data-rets-mapper-id="' + hiddenItem +'"]' ).hide();
+        console.info( 'wp-rets-client', 'hiding field', hiddenItem );
+      });
+
+    }
+
+      jQuery( "#rets-mapper-table" ).tablesorter( {
         cssAsc: 'asc',
         cssDesc: 'desc',
         widthFixed: true
-      });
-      jQuery('.mapper-show-info').on('click', function(){
-        var current_class = '.' + jQuery(this).data('info-block');
-        jQuery(current_class).toggle();
-      });
+      } );
+      jQuery( '.mapper-show-info' ).on( 'click', function () {
+        var current_class = '.' + jQuery( this ).data( 'info-block' );
+        jQuery( current_class ).toggle();
+      } );
     }
   );
 
