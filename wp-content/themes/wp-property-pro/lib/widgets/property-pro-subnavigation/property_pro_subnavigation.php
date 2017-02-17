c8157df73979a472c44fda85e7dc76c86691c2f2
dmitriy <fq.jony@gmail.com>
2017-02-17 11:56:51 +0200

    add namespaces, use get_template_directory_uri instead of get_stylesheet_directory_uri https://github.com/UsabilityDynamics/www.reddoorcompany.com/pull/911

--- wp-content/themes/wp-property-pro/lib/widgets/property-pro-subnavigation/property_pro_subnavigation.php
+++ wp-content/themes/wp-property-pro/lib/widgets/property-pro-subnavigation/property_pro_subnavigation.php
@@ -6,72 +6,76 @@ Author: fq.jony@UD
 Author URI:
 */
 
-if (!class_exists('SiteOrigin_Widget')) include_once get_stylesheet_directory() . '/lib/widgets/property-pro-subnavigation/base/property-pro-siteorigin-widget.class.php';
 
-class Property_Pro_Subnavigation_Widget extends SiteOrigin_Widget
-{
+namespace UsabilityDynamics\PropertyPro\PropertyProSubnavigation {
 
-  function __construct()
+  if (!class_exists('SiteOrigin_Widget')) include_once get_stylesheet_directory() . '/lib/widgets/property-pro-subnavigation/base/property-pro-siteorigin-widget.class.php';
+
+  class Property_Pro_Subnavigation_Widget extends \SiteOrigin_Widget
   {
 
-    parent::__construct(
-      'porperty-pro-subnavigation',
-      __('Property Pro Subnavigation', 'so-widgets-bundle'),
-      [
-        'description' => __('A customizable subnavigation widget.', 'so-widgets-bundle'),
-        'help' => 'https://siteorigin.com/widgets-bundle/button-widget-documentation/'
-      ],
-      [],
-      false,
-      get_stylesheet_directory_uri('/lib/widgets/property-pro-subnavigation')
-    );
+    function __construct()
+    {
 
-  }
-
-  function initialize()
-  {
-    $this->register_frontend_styles(
-      [
+      parent::__construct(
+        'porperty-pro-subnavigation',
+        __('Property Pro Subnavigation', 'wp-property-pro'),
         [
-          'property-pro-subnavigation-base',
-          get_stylesheet_directory_uri('/lib/widgets/property-pro-subnavigation/css/style.css'),
-          [],
-          SOW_BUNDLE_VERSION
+          'description' => __('A customizable subnavigation widget.', 'wp-property-pro'),
+          'help' => 'https://siteorigin.com/widgets-bundle/button-widget-documentation/'
         ],
-      ]
-    );
-  }
+        [],
+        false,
+        get_template_directory().'/lib/widgets/property-pro-subnavigation'
+      );
 
-  function initialize_form()
-  {
-    $menus = [];
-    foreach (get_terms('nav_menu', array('hide_empty' => true)) as $menu) {
-      $menus[$menu->term_id] = $menu->name;
     }
 
-    return [
-      'layout' => [
-        'type' => 'select',
-        'label' => __('Layout', 'so-widgets-bundle'),
-        'default' => 'text',
-        'options' => [
-          'text' => __('Text', 'so-widgets-bundle'),
-          'icon' => __('Icon', 'so-widgets-bundle')
+    function initialize()
+    {
+      $this->register_frontend_styles(
+        [
+          [
+            'property-pro-subnavigation-base',
+            get_template_directory().'/lib/widgets/property-pro-subnavigation/css/style.css',
+            [],
+            SOW_BUNDLE_VERSION
+          ],
+        ]
+      );
+    }
+
+    function initialize_form()
+    {
+      $menus = [];
+      foreach (get_terms('nav_menu', array('hide_empty' => true)) as $menu) {
+        $menus[$menu->term_id] = $menu->name;
+      }
+
+      return [
+        'layout' => [
+          'type' => 'select',
+          'label' => __('Layout', 'wp-property-pro'),
+          'default' => 'text',
+          'options' => [
+            'text' => __('Text', 'wp-property-pro'),
+            'icon' => __('Icon', 'wp-property-pro')
+          ],
         ],
-      ],
-      'menu_select' => [
-        'type' => 'select',
-        'label' => __('Menu select', 'so-widgets-bundle'),
-        'options' => $menus
-      ]
-    ];
+        'menu_select' => [
+          'type' => 'select',
+          'label' => __('Menu select', 'wp-property-pro'),
+          'options' => $menus
+        ]
+      ];
+    }
+
+    function get_template_name($instance)
+    {
+      return 'base';
+    }
+
   }
 
-  function get_template_name($instance)
-  {
-    return 'base';
-  }
-
+  siteorigin_widget_register('property-pro-subnavigation', __FILE__, 'UsabilityDynamics\PropertyPro\PropertyProSubnavigation\Property_Pro_Subnavigation_Widget');
 }
-
-siteorigin_widget_register('property-pro-subnavigation', __FILE__, 'Property_Pro_Subnavigation_Widget');
