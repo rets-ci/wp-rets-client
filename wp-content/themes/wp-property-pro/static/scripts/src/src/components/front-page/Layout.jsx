ebba115c075ce82dfbd78346bd8c7c9439c57a9a
dmitriy <fq.jony@gmail.com>
2017-02-22 14:50:51 +0200

    move footer components to front-page directory https://github.com/UsabilityDynamics/www.reddoorcompany.com/issues/926

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/front-page/Layout.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/front-page/Layout.jsx
@@ -4,7 +4,7 @@ import UserPanel from './UserPanel.jsx';
 import Masthead from '../widgets/masthead/Masthead.jsx';
 import Testimonials from '../widgets/testimonials/Testimonials.jsx';
 import Subnavigation from '../widgets/subnavigation/Subnavigation.jsx';
-import Footer from '../Footer.jsx';
+import Footer from './Footer.jsx';
 
 export default class Home extends Component {
     static propTypes = {
