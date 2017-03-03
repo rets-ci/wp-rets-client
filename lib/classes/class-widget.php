56821beea0fb03e4cf2526029834fb693add56fc
pemaxim <maxim.peshkov@usabilitydynamics.com>
2017-03-03 17:47:35 +0200

    removed derpeciated stuff

--- lib/classes/class-widget.php
+++ lib/classes/class-widget.php
@@ -31,54 +31,11 @@ namespace UsabilityDynamics\WPRETSC {
       }
 
       /**
-       *
-       */
-      private function is_ud_site_id_registered() {
-        return get_site_option( 'ud_site_id' );
-      }
-
-      /**
-       *
-       */
-      private function is_ud_site_secret_token_registered() {
-        return get_site_option( 'ud_site_secret_token' );
-      }
-
-      /**
-       * @return mixed
-       */
-      private function is_ud_site_public_key_registered() {
-        return get_site_option( 'ud_site_public_key' );
-      }
-
-      /**
-       * @return mixed
-       */
-      private function is_ud_site_id_registered() {
-        return get_site_option( 'ud_site_id' );
-      }
-
-      /**
-       * @return mixed
-       */
-      private function is_ud_site_secret_token_registered() {
-        return get_site_option( 'ud_site_secret_token' );
-      }
-
-      /**
        * @param \UsabilityDynamics\WPP\type $args
        * @param \UsabilityDynamics\WPP\type $instance
        * @return string
        */
       public function widget($args, $instance) {
-
-        /**
-         * Do nothing if was not able to register on UD
-         */
-//        if ( !$this->is_ud_site_id_registered() || !$this->is_ud_site_secret_token_registered() ) {
-//          echo 'System is not ready for RETS.CI. Refresh page or contact UsabilityDynamics, Inc.';
-//          return;
-//        }
 
         if( defined( 'UD_RETSCI_AJAX_API_URL' ) ) {
           $api_url = trailingslashit(UD_RETSCI_AJAX_API_URL);
@@ -88,48 +45,17 @@ namespace UsabilityDynamics\WPRETSC {
 
         wp_enqueue_script( 'wpp_retsci_app', ud_get_wp_rets_client()->path( 'static/scripts/app.js', 'url' ), array( 'jquery' ) );
 
-        /*
-
-        $data = json_encode(array(
-          'ud_site_id' => $this->is_ud_site_id_registered(),
-          'ud_site_secret_token' => $this->is_ud_site_secret_token_registered(),
-          'user_data' => get_userdata(get_current_user_id()),
-          'blog_id' => get_current_blog_id(),
-          'security' => wp_create_nonce( "wpp_retsci_subscription" ),
-          'api_url' => $api_url
-        ));
-
-        ob_start();
-        include_once ud_get_wp_rets_client()->path( 'static/views/widget-subscription.php', 'dir' );
-        echo apply_filters( 'wpp_retsci_widget_subscription_content', ob_get_clean() );
-        */
-
-        /**
-         * If not registered on rets ci yet
-         */
-        if ( !$this->is_ud_site_id_registered() || !$this->is_ud_site_secret_token_registered() || !$this->is_ud_site_public_key_registered() ) {
-
-          $data = json_encode(array(
-            'ud_site_id' => $this->is_ud_site_id_registered(),
-            'ud_site_secret_token' => $this->is_ud_site_secret_token_registered(),
-            'ud_site_secret_token' => md5( wp_generate_password() ),
-            'security' => wp_create_nonce( "wpp_retsci_signin" ),
-            'api_url' => $api_url
-          ));
-
-          ob_start();
-          include_once ud_get_wp_rets_client()->path( 'static/views/widget-register.php', 'dir' );
-          echo apply_filters( 'wpp_retsci_widget_register_content', ob_get_clean() );
-
-          return;
-        }
+        wp_localize_script( 'wpp_retsci_app', 'wpp_retsci_client', array(
+          'timezone' => get_option( 'timezone_string' ),
+          'time_format' => get_option( 'time_format' ),
+        ) );
 
         /**
          * Show stats
          */
         $data = json_encode(array(
-          'site_id' => $this->is_ud_site_id_registered(),
-          'site_secret_token' => $this->is_ud_site_secret_token_registered(),
+          'site_id' => get_site_option( 'ud_site_id' ),
+          'site_secret_token' => get_site_option( 'ud_site_secret_token' ),
           'api_url' => $api_url
         ));
 
