8cd884b6a3498212a8e1fc066103efa10badeaee
ashkan shahabi <ashkanshahabi@gmail.com>
2017-03-05 11:05:19 +0000

    Integrated Swiper as the default carousel, used it for the home page and each individual card, ref #869

---
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/widgets/listing_carousel/layouts/swiper/Index.jsx
@@ -0,0 +1,69 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import _ from 'lodash';
+import PropertyCard from './PropertyCard.jsx';
+import Swiper from 'swiper';
+
+require('swiper-css');
+
+export default class DefaultLayout extends Component {
+  static propTypes = {
+    item: PropTypes.object
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+   this.swiper = new Swiper(this.swiperElement, {
+     slidesPerView: 'auto',
+     nextButton: this.swiperElementNext,
+     prevButton: this.swiperElementPrev,
+     spaceBetween: 20
+   });
+  }
+
+  render() {
+    let {
+      item
+    } = this.props;
+    let posts = _.get(item, 'posts', []);
+    return (
+      <div className="container">
+
+        {
+          _.get(item, 'title', null)
+            ? <h3>{_.get(item, 'title')}</h3>
+            : null
+        }
+        {
+          _.get(item, 'subtitle', null)
+            ? <p>{_.get(item, 'subtitle')}</p>
+            : null
+        }
+        {
+          posts.length
+            ?
+              <div className="listing-carousel clearfix">
+                <div className="swiper-container" ref={(r) => this.swiperElement = r} style={{width: '100%', height: '400px'}}>
+                  <div className="swiper-wrapper">
+                    {
+                      posts.map((post, key) => {
+                        return (
+                          <PropertyCard data={post} key={key} />
+                        )
+                      })
+                    }
+                    </div>
+                </div>
+              </div>
+            : null
+        }
+        <div className="listing-control-nav">
+          <a href="#" className="prev-nav" ref={(r) => this.swiperElementPrev = r}><i className="fa fa-angle-left"></i></a>
+          <a href="#" className="next-nav" ref={(r) => this.swiperElementNext = r}><i className="fa fa-angle-right"></i></a>
+        </div>
+      </div>
+    );
+  }
+};
--- wp-content/themes/wp-property-pro/static/scripts/src/webpack.config.js
+++ wp-content/themes/wp-property-pro/static/scripts/src/webpack.config.js
@@ -1,3 +1,6 @@
+
+const path = require('path');
+
 module.exports = {
     entry: './src/index.jsx',
     output: {
@@ -5,25 +8,35 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        preLoaders: [
-            { test: /\.json$/, loader: 'json'},
-        ],
         loaders: [
-            {
-              test: /\.jsx$/,
-              exclude: /node_modules/,
-              loader: 'babel',
-              query: { presets: ['es2015', 'react', 'stage-0'] }
-            },
-            {
-              test: /\.(ico|png|jpg)(\?.*)?$/,
-              exclude: /node_modules/,
-              loader: "file?name=images/[name].[ext]"},
-            {
-              test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-              exclude: /node_modules/,
-              loader: 'url?limit=10000&mimetype=image/svg+xml'
-            }
+          {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+          },
+          {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: { presets: ['es2015', 'react', 'stage-0'] }
+          },
+          {
+            test: /\.json$/, loader: 'json'
+          },
+          {
+            test: /\.(ico|png|jpg)(\?.*)?$/,
+            exclude: /node_modules/,
+            loader: "file?name=images/[name].[ext]"
+          },
+          {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            exclude: /node_modules/,
+            loader: 'url?limit=10000&mimetype=image/svg+xml'
+          }
         ]
+    },
+    resolve: {
+      alias: {
+        'swiper-css': path.join(__dirname, '/node_modules/swiper/dist/css/swiper.min.css')
+      }
     }
 };
