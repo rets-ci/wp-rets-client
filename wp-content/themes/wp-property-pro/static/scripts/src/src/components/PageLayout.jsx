917d0ae125b562a9538af6ed56f5a7072f5d934e
dmitriy <fq.jony@gmail.com>
2017-03-16 13:10:33 +0200

    error handler for get page content request, remove unneeded method, some fixes https://github.com/UsabilityDynamics/www.reddoorcompany.com/pull/980/files

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/PageLayout.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/PageLayout.jsx
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import Header from './Header.jsx';
 import LoadingAccordion from './LoadingAccordion.jsx';
 import UserPanel from './UserPanel.jsx';
-import Util from './Util.jsx';
+import _ from 'lodash';
 
 export default class PageLayout extends Component {
   static propTypes = {
@@ -17,8 +17,23 @@ export default class PageLayout extends Component {
   }
 
   componentDidMount() {
-    Util.getPageContent(window.location.pathname, (data) => {
-      this.setState({post: data.post});
+
+    // Get page content query
+    let url = window.location.pathname + window.location.search;
+    jQuery.ajax({
+      url: url,
+      type: 'get',
+      data: {
+        pageType: 'json'
+      },
+      dataType: 'json',
+      success: function (data) {
+        if(_.get(data, 'post', null))
+          this.setState({post: data.post});
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
     });
   }
 
