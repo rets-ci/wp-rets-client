b8f4ac731b37822e7086f57c40c62d8bc4ad9a41
dmitriy <fq.jony@gmail.com>
2017-03-30 18:07:52 +0300

    update `next article` link, fix children posts count display https://github.com/UsabilityDynamics/www.reddoorcompany.com/issues/993

--- wp-content/themes/wp-property-pro/lib/classes/class-property-pro.php
+++ wp-content/themes/wp-property-pro/lib/classes/class-property-pro.php
@@ -176,7 +176,37 @@ namespace UsabilityDynamics {
           $params['post']['guide_single_content']['parent_category_relative_link'] = str_replace(home_url(), "", get_term_link($term_parent, $guide_category));
 
           /** Get next guide relative link */
-          $next_post = get_previous_post();
+
+          /** Get all posts ids in guide terms */
+          $termIds = get_term_children($term_parent, $guide_category);
+          $ids = [];
+          foreach ($termIds as $termId) {
+            $ids = array_merge($ids, get_posts([
+              'post_type' => $guide_post_type,
+              'posts_per_page' => -1,
+              'fields' => 'ids',
+              'tax_query' => [
+                [
+                  'taxonomy' => $guide_category,
+                  'field' => 'id',
+                  'terms' => $termId
+                ]
+              ]
+            ]));
+          }
+
+          $next_post = null;
+          if ($ids) {
+            /** Get current post index */
+            $current_post_index = array_search($post->ID, $ids);
+
+            /** Get next post index */
+            $next_index = is_numeric($current_post_index) ? $current_post_index + 1 : 0;
+
+            /** Get next post data */
+            $next_post = get_post(isset($ids[$next_index]) ? $ids[$next_index] : $ids[0]);
+          }
+
           $params['post']['guide_single_content']['next_article_relative_link'] = $next_post ? str_replace(home_url(), "", get_permalink($next_post->ID)) : '';
 
           /** Masthead widget payload */
@@ -335,6 +365,7 @@ namespace UsabilityDynamics {
               ];
             },get_posts([
               'post_type' => 'propertypro-guide',
+              'posts_per_page' => -1,
               'tax_query' => [
                 [
                   'taxonomy' => 'propertypro-guide-category',
