c5a4db3e6cc1ef62878820c0ad39c8913156101b
ashkan shahabi <ashkanshahabi@gmail.com>
2017-07-11 22:39:12 +0200

    Fixed an issue where no results weren’t handled correctly in map search results page, and other fixes ref #1126

--- wp-content/themes/wp-property-pro/static/scripts/src/src/reducers/errorMessage.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/reducers/errorMessage.jsx
@@ -2,9 +2,9 @@ import {Lib} from "../lib.jsx";
 
 const errorMessage = (state = null, action) => {
   const { type, error } = action;
-  if (type === Lib.RESET_ERROR_MESSAGE) {
+  if (type === Lib.RESET_ERROR_MESSAGE_ACTION) {
     return null;
-  } else if (error && Lib.ERROR_MESSAGE) {
+  } else if (error && Lib.ERROR_MESSAGE_ACTION) {
     return error;
   }
   return state;
