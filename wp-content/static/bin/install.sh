d2b2ff5f6263458e3a20475c14f0384bff503148
Ash Shahabi <ashkanshahabi@gmail.com>
2017-08-15 21:08:02 +0200

    Some improvements on the previous PR (#1298)
    * Made the install.sh script cleaner
    
    * Made login modal centre horizontally and vertically, ref #1209

--- wp-content/static/bin/install.sh
+++ wp-content/static/bin/install.sh
@@ -2,8 +2,7 @@ if [[ ! -z "$GIT_BRANCH" ]]
 then
 	if [ "$GIT_BRANCH" != "production" ] && [ "$GIT_BRANCH" != "latest-v3" ];
 	then
-    cd /var/www/wp-content/themes/wp-property-pro/static/scripts/src
-    NODE_ENV=development npm install
+	NODE_ENV=development npm --prefix=/var/www/wp-content/themes/wp-property-pro/static/scripts/src install
 	fi
 fi
 
