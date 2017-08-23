2dddd6f47b358726d91bb35c3dc82c3d0aa0a27a
ashkan shahabi <ashkanshahabi@gmail.com>
2017-08-23 10:23:14 +0200

    modified install.sh file to test file installation

--- wp-content/static/bin/install.sh
+++ wp-content/static/bin/install.sh
@@ -2,7 +2,9 @@ if [[ ! -z "$GIT_BRANCH" ]]
 then
 	if [ "$GIT_BRANCH" != "production" ] && [ "$GIT_BRANCH" != "latest-v3" ];
 	then
-	NODE_ENV=development npm --prefix=/var/www/wp-content/themes/wp-property-pro/static/scripts/src install
+		cd /var/www/wp-content/themes/wp-property-pro/static/scripts/src
+		NODE_ENV=development npm install
+		touch testing-install-script
 	fi
 fi
 
