cbaee616535168374cb42991c963b61552f64e12
RumenTech <rumen.buhalov@gmail.com>
2017-08-28 17:53:10 +0300

    card should act like a link

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertyCard.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertyCard.jsx
@@ -91,69 +91,70 @@ export default class PropertyCard extends Component {
     return (
       <div
         className={classes.join(' ')}>
-        <div className={Lib.THEME_CLASSES_PREFIX + "card-img"}>
-          <div className={Lib.THEME_CLASSES_PREFIX + "card-img-top"}>
-            <div className={Lib.THEME_CLASSES_PREFIX + "listing-top"}>
-              <span className={Lib.THEME_CLASSES_PREFIX + "price"}>{Util.formatPriceValue(price)}</span>
-              <span className={Lib.THEME_CLASSES_PREFIX + "action-btn-group"}>
-                {/*<a href="#" className={`${Lib.THEME_CLASSES_PREFIX}favorite ${Lib.THEME_CLASSES_PREFIX}active`}
-                  title="Save as favorite">
-                  <i className="fa fa-heart" aria-hidden="true"></i>
-                </a>
-                <a href="#" className={Lib.THEME_CLASSES_PREFIX + "hide"} title="Hide">
-                  <i className="fa fa-eye-slash" aria-hidden="true"></i>
-                </a>*/}
-              </span>
-            </div>
-            <Swiper {...swiperParams}>
-              <div className="swiper-slide" key={0}>
-                <img
-                  alt="Card image cap"
-                  className="swiper-lazy card-img-top"
-                  src={_.isEmpty(thumbnail) ? Util.getGoogleStreetViewThumbnailURL({
-                      size: Lib.PROPERTY_LISTING_IMAGE_SIZE,
-                      location: !_.isEmpty(location) ? location.join(',') : ''
-                    }) : (!_.get(this.props.data, 'full_image', false) ? Util.getThumbnailUrlBySize(thumbnail, Lib.PROPERTY_LISTING_IMAGE_SIZE) : thumbnail)}
-                />
-              </div>
-              {gallery_images.slice(1).map((d, k) =>
-                <div className="swiper-slide" key={k + 1}>
-                  <img
-                    alt={_.isEmpty(d) ? 'Card image cap' : ''}
-                    className="swiper-lazy card-img-top"
-                    data-src={Util.getThumbnailUrlBySize(d, Lib.PROPERTY_LISTING_IMAGE_SIZE)}
-                  />
-                  <div className="swiper-lazy-preloader"></div>
-                </div>
-              )}
-            </Swiper>
-          </div>
-          <div className={Lib.THEME_CLASSES_PREFIX + "direction-nav-container"}>
-            <ul className={`nav ${Lib.THEME_CLASSES_PREFIX}direction-nav text-center`}>
-              <li className="nav-item mr-auto">
-                <a href="#"
-                  className={`${Lib.THEME_CLASSES_PREFIX}nav-prev rounded-circle`}
-                  onClick={e => {
-                    e.preventDefault();
-                    this.handleNavigation.bind(this)('prev');
-                  }}
-                ></a>
-              </li>
-              <li className="nav-item">
-                <a href="#"
-                  className={`${Lib.THEME_CLASSES_PREFIX}nav-next rounded-circle`}
-                  onClick={e => {
-                    e.preventDefault();
-                    this.handleNavigation.bind(this)('next');
-                  }}
-                ></a>
-              </li>
-            </ul>
-          </div>
-        </div>
         <Link
           to={link}
         >
+          <div className={Lib.THEME_CLASSES_PREFIX + "card-img"}>
+            <div className={Lib.THEME_CLASSES_PREFIX + "card-img-top"}>
+              <div className={Lib.THEME_CLASSES_PREFIX + "listing-top"}>
+                <span className={Lib.THEME_CLASSES_PREFIX + "price"}>{Util.formatPriceValue(price)}</span>
+                <span className={Lib.THEME_CLASSES_PREFIX + "action-btn-group"}>
+                  {/*<a href="#" className={`${Lib.THEME_CLASSES_PREFIX}favorite ${Lib.THEME_CLASSES_PREFIX}active`}
+                    title="Save as favorite">
+                    <i className="fa fa-heart" aria-hidden="true"></i>
+                  </a>
+                  <a href="#" className={Lib.THEME_CLASSES_PREFIX + "hide"} title="Hide">
+                    <i className="fa fa-eye-slash" aria-hidden="true"></i>
+                  </a>*/}
+                </span>
+              </div>
+              <Swiper {...swiperParams}>
+                <div className="swiper-slide" key={0}>
+                  <img
+                    alt="Card image cap"
+                    className="swiper-lazy card-img-top"
+                    src={_.isEmpty(thumbnail) ? Util.getGoogleStreetViewThumbnailURL({
+                        size: Lib.PROPERTY_LISTING_IMAGE_SIZE,
+                        location: !_.isEmpty(location) ? location.join(',') : ''
+                      }) : (!_.get(this.props.data, 'full_image', false) ? Util.getThumbnailUrlBySize(thumbnail, Lib.PROPERTY_LISTING_IMAGE_SIZE) : thumbnail)}
+                  />
+                </div>
+                {gallery_images.slice(1).map((d, k) =>
+                  <div className="swiper-slide" key={k + 1}>
+                    <img
+                      alt={_.isEmpty(d) ? 'Card image cap' : ''}
+                      className="swiper-lazy card-img-top"
+                      data-src={Util.getThumbnailUrlBySize(d, Lib.PROPERTY_LISTING_IMAGE_SIZE)}
+                    />
+                    <div className="swiper-lazy-preloader"></div>
+                  </div>
+                )}
+              </Swiper>
+            </div>
+            <div className={Lib.THEME_CLASSES_PREFIX + "direction-nav-container"}>
+              <ul className={`nav ${Lib.THEME_CLASSES_PREFIX}direction-nav text-center`}>
+                <li className="nav-item mr-auto">
+                  <a href="#"
+                    className={`${Lib.THEME_CLASSES_PREFIX}nav-prev rounded-circle`}
+                    onClick={e => {
+                      e.preventDefault();
+                      this.handleNavigation.bind(this)('prev');
+                    }}
+                  ></a>
+                </li>
+                <li className="nav-item">
+                  <a href="#"
+                    className={`${Lib.THEME_CLASSES_PREFIX}nav-next rounded-circle`}
+                    onClick={e => {
+                      e.preventDefault();
+                      this.handleNavigation.bind(this)('next');
+                    }}
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
           <div className={`card-block ${Lib.THEME_CLASSES_PREFIX}card-block`}>
             <h4 className={`card-title ${Lib.THEME_CLASSES_PREFIX}card-title m-0`}>{address}</h4>
             <p className={`card-text ${Lib.THEME_CLASSES_PREFIX}card-text`}>{city}, {state} {zip}</p>
