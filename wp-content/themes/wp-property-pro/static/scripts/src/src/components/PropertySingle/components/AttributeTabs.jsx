bfe95c20521339e4c760e203f079009ea1b67ac9
ashkan shahabi <ashkanshahabi@gmail.com>
2017-11-07 10:05:02 +0100

    Added All tab to property details, ref #1451

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertySingle/components/AttributeTabs.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertySingle/components/AttributeTabs.jsx
@@ -12,37 +12,18 @@ import esSchema from 'app_root/static_data/property-data-structure/index.js';
 const LISTING_TYPES_TO_HIDE = [ 'commercial', 'land' ];
 const descriptionBoilerplate = '847 Estes Street is a house for rent in Durham, NC 27701. This 1440 square foot house sits on a 0.13 lot and features 3 bedrooms and 2 bathrooms. Built in 1915, this house has been on the market for a total of 1 month and is currently priced at $1,100 a month.';
 
-const getAllTabData = (propertyData, colNumbers) => {
-  var allItems = [];
-  Object.keys(propertyData).forEach((tabs) => {
-    propertyData[tabs].forEach(cols => {
-      Object.keys(cols).forEach(item => {
-        allItems.push({[item]: cols[item]});
-      })
-    });
+const getAllTabData = (propertyDataStructure) => {
+  let AllTab = {
+    "name": "All",
+    "children": []
+  };
+  let combinedChildren = propertyDataStructure.map(d => {
+    return d.children;
+  }).reduce((a, b) => {
+    return a.concat(b);
   });
-
-  let itemsPerCol = Math.floor(allItems.length / colNumbers);
-  let colsArray = [];
-  for (var i = 0; i < colNumbers; i++) {
-    (function () {
-      let obj = {};
-      let itemPerColLocal = itemsPerCol;
-      while(itemPerColLocal > 0) {
-        let poppedItem = allItems.shift();
-        merge(obj, poppedItem);
-        itemPerColLocal--;
-      }
-      if (i === (colNumbers - 1)) {
-        allItems.forEach(a => {
-          merge(obj, a);
-        });
-      }
-      colsArray.push(obj);
-    })(itemsPerCol)
-  }
-
-  return colsArray;
+  AllTab['children'] = combinedChildren;
+  return AllTab;
 };
 
 class AttributeTabs extends Component {
@@ -50,7 +31,7 @@ class AttributeTabs extends Component {
     super(props);
 
     this.state = {
-      selectedTab: 'Rooms'
+      selectedTab: 'All'
     };
   }
 
@@ -76,10 +57,8 @@ class AttributeTabs extends Component {
     }
 
     let propertyDataStructure = esSchema[listingTypeJSONFileName];
-
-    // let schemaModified = Object.assign({}, esSchema);
-    // schemaModified['All'] = getAllTabData(schemaModified, 2);
-    // console.log('property info tabs: ', schemaModified);
+    let allTab = getAllTabData(propertyDataStructure.slice(0));
+    propertyDataStructure.push(allTab);
 
     let tabs = propertyDataStructure.map(p => p.name);
     let content = propertyDataStructure.find(d => d.name === selectedTab);
