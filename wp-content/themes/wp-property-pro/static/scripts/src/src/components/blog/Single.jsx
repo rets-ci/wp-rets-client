5cd0ccc0222dc78726cee42e58439bc0dc207b5c
fqjony <fq.jony@gmail.com>
2017-11-10 16:36:04 +0200

    added toolbar to single blog page ref #1605

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/blog/Single.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/blog/Single.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import HeaderDefault from 'app_root/components/Headers/HeaderDefault.jsx';
 import Masthead from '../widgets/masthead/Masthead.jsx';
 import PostCard from './components/PostCard.jsx';
 import PostContent from './components/PostContent.jsx';
@@ -12,6 +13,12 @@ class Single extends Component {
   };
 
   render() {
+
+    let {
+      history,
+      openUserPanel,
+      openLoginModal
+    } = this.props;
 
     let posts = get(this.props.post, 'related_posts', []);
     let groups = [];
@@ -29,50 +36,56 @@ class Single extends Component {
     }
 
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <article>
+      <div>
+        <section
+          className={`${Lib.THEME_CLASSES_PREFIX}toolbar ${Lib.THEME_CLASSES_PREFIX}header-default row no-gutters`}>
+          <HeaderDefault historyPush={history.push} openUserPanel={openUserPanel} openLoginModal={openLoginModal}/>
+        </section>
+        <div className="container-fluid">
+          <div className="row">
+            <article>
+              {
+                get(this.props.post, 'widgets.masthead', null)
+                  ? <Masthead widget_cell={get(this.props.post, 'widgets.masthead')}/>
+                  : null
+              }
+              {
+                get(this.props.post, 'content', null)
+                  ? <PostContent content={this.props.post.content}/>
+                  : null
+              }
+            </article>
             {
-              get(this.props.post, 'widgets.masthead', null)
-                ? <Masthead widget_cell={get(this.props.post, 'widgets.masthead')}/>
+              get(this.props.post, 'related_posts', []).length
+                ?
+                <section className={Lib.THEME_CLASSES_PREFIX + "related-posts"}>
+                  <div className="container">
+                    <div className="row">
+                      {
+                        get(this.props.post, 'category_title', null) && get(this.props.post, 'related_posts', []).length
+                          ? <div className={`${Lib.THEME_CLASSES_PREFIX}more-posts text-center`}>
+                            <h4>More {this.props.post.category_title} Articles</h4>
+                          </div>
+                          : null
+                      }
+                    </div>
+                    <div className="row">
+                      {
+                        groups.map((g, group_index) => {
+                            let groupPosts = g.map((p, i) =>
+                              <PostCard data={p} key={i}/>
+                            );
+                            return (<div className={`card-deck ${Lib.THEME_CLASSES_PREFIX}blog-posts-row`}
+                                         key={group_index}>{groupPosts}</div>);
+                          }
+                        )
+                      }
+                    </div>
+                  </div>
+                </section>
                 : null
             }
-            {
-              get(this.props.post, 'content', null)
-                ? <PostContent content={this.props.post.content}/>
-                : null
-            }
-          </article>
-          {
-            get(this.props.post, 'related_posts', []).length
-              ?
-              <section className={Lib.THEME_CLASSES_PREFIX + "related-posts"}>
-                <div className="container">
-                  <div className="row">
-                    {
-                      get(this.props.post, 'category_title', null) && get(this.props.post, 'related_posts', []).length
-                        ? <div className={`${Lib.THEME_CLASSES_PREFIX}more-posts text-center`}>
-                          <h4>More {this.props.post.category_title} Articles</h4>
-                        </div>
-                        : null
-                    }
-                  </div>
-                  <div className="row">
-                    {
-                      groups.map((g, group_index) => {
-                          let groupPosts = g.map((p, i) =>
-                            <PostCard data={p} key={i}/>
-                          );
-                          return (<div className={`card-deck ${Lib.THEME_CLASSES_PREFIX}blog-posts-row`}
-                                       key={group_index}>{groupPosts}</div>);
-                        }
-                      )
-                    }
-                  </div>
-                </div>
-              </section>
-              : null
-          }
+          </div>
         </div>
       </div>
     )
