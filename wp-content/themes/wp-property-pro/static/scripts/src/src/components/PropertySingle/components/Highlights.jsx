da290519a89b6d43c6431e9b9862c0353724e736
RumenTech <rumen.buhalov@gmail.com>
2017-11-15 20:58:52 +0200

    updated attributes on overview/highlights

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertySingle/components/Highlights.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/PropertySingle/components/Highlights.jsx
@@ -27,71 +27,67 @@ const Highlights = ({ elasticSearchSource, fromMapView }) => {
     return null;
   }
 
-  let gridClass = `${Lib.THEME_CLASSES_PREFIX}small-info-box `;
-  if (fromMapView) {
-    gridClass += 'col-6 col-xl-4'
-  } else {
-    gridClass += 'col-4 col-md-3'
-  }
+  let boxClass = `${Lib.THEME_CLASSES_PREFIX}attr-box`;
+  let gridClass = fromMapView ? 'col-6' : 'col-6 col-md-4';
 
-  const subTypes = getSubTypes(elasticSearchSource) || 'N/A'
-  const design = getDesign(elasticSearchSource) || 'N/A'
-  const style = getStyle(elasticSearchSource) || 'N/A'
-  const yearBuilt = getYearBuilt(elasticSearchSource) || 'N/A'
+  const subTypes          = getSubTypes(elasticSearchSource) || 'N/A'
+  const design            = getDesign(elasticSearchSource) || 'N/A'
+  const style             = getStyle(elasticSearchSource) || 'N/A'
+  const yearBuilt         = getYearBuilt(elasticSearchSource) || 'N/A'
   const isNewConstruction = getNewConstruction(elasticSearchSource)
-  const subDivision = getSubdivision(elasticSearchSource) || 'N/A'
-  const insideCity = getInsideCity(elasticSearchSource)
-  const county = getCounty(elasticSearchSource) || 'N/A'
-  const elementarySchool = getElementarySchool(elasticSearchSource) || 'N/A'
-  const middleSchool = getMiddleSchool(elasticSearchSource) || 'N/A'
-  const highSchool = getMiddleSchool(elasticSearchSource) || 'N/A'
+  const subDivision       = getSubdivision(elasticSearchSource) || 'N/A'
+  const insideCity        = getInsideCity(elasticSearchSource)
+  const county            = getCounty(elasticSearchSource) || 'N/A'
+  const elementarySchool  = getElementarySchool(elasticSearchSource) || 'N/A'
+  const middleSchool      = getMiddleSchool(elasticSearchSource) || 'N/A'
+  const highSchool        = getMiddleSchool(elasticSearchSource) || 'N/A'
 
   const designAndStyle = [ design, style ].join(', ')
   const yearAndNewFlag = isNewConstruction === 'Yes' ? `${yearBuilt}, New Construction` : yearBuilt
 
   return (
     <div className={ `${Lib.THEME_CLASSES_PREFIX}single-highlights pt-5` }>
-      <h5 className={ `${Lib.THEME_CLASSES_PREFIX}info-section-header mb-4` }>
+      <h5 className={ `${Lib.THEME_CLASSES_PREFIX}info-section-header` }>
         Property Highlights
       </h5>
 
-      <div className="row pt-3">
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Type </p>
+      <div className={ `${Lib.THEME_CLASSES_PREFIX}attr-box-container row` }>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Type </p>
           <p>{ subTypes }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Style </p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Style </p>
           <p>{ designAndStyle }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Year </p>
-          <p>{ yearAndNewFlag || 'N/A' }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Subdivision</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Year </p>
+          <p>{ yearAndNewFlag }</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Subdivision</p>
           <p>{ subDivision }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Inside City</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Inside City</p>
           <p>{ insideCity }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">County</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>County</p>
           <p>{ county }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Elementary School</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Elementary School</p>
           <p>{ elementarySchool }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">Middle School</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>Middle School</p>
           <p>{ middleSchool }</p>
-        </div>
-        <div className={ `${gridClass}` }>
-          <p className="text-muted">High School</p>
+        </div></div>
+        <div className={ gridClass }><div className={ boxClass }>
+          <p>High School</p>
           <p>{ highSchool }</p>
-        </div>
+        </div></div>
       </div>
     </div>
   );
