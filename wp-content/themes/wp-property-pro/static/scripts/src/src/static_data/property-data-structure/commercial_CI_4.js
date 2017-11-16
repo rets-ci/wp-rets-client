9387b77879ecff8cf1ec97f88dc6af647f1187f5
ashkan shahabi <ashkanshahabi@gmail.com>
2017-11-16 15:34:28 +0100

    Added missing attributes to property single details and relevant changes ref #1451

--- wp-content/themes/wp-property-pro/static/scripts/src/src/static_data/property-data-structure/commercial_CI_4.js
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/static_data/property-data-structure/commercial_CI_4.js
@@ -7,6 +7,7 @@ import {
   getCounty,
   getCrossStreet,
   getDirections,
+  getEstFinMonth,
   getFoundation,
   getInsideCity,
   getLotSizeArea,
@@ -17,6 +18,7 @@ import {
   getRoadFrontage,
   getRoof,
   getLatitude,
+  getListingStatuses,
   getLongitude,
   getLotSizeDim,
   getMLSId,
@@ -41,7 +43,7 @@ export default [
       {"name": "Interior", "items": [
         {"name": "Unit Count", "value": (data) => { return get(data, 'post_meta.rets_number_of_units[0]', null); }, "order": 1},
         {"name": "Ceiling Height", "value": (data) => { return get(data, 'post_meta.rets_ceiling_height[0]', null); }, "order": 2},
-        {"name": "Sprinklers", "value": (data) => { return null; }, "order": 3},
+        {"name": "Sprinklers", "value": (data) => { return get(data, 'tax_input.rets_sprinklers.rets_sprinklers', []).map(d => d.name).join(', ') || null; }, "order": 3},
       ], "order": 1},
       {"name": "Exterior", "items": [
         {"name": "Roof", "value": (data) => { return getRoof(data); }, "order": 1},
@@ -65,7 +67,7 @@ export default [
   {"name": "Property", "children": [
     {"name": "Building", "items": [
       {"name": "New Construction", "value": (data) => { return getNewConstruction(data); }, "order": 1},
-      {"name": "Construction Completion", "value": (data) => { return null; }, "order": 2},
+      {"name": "Construction Completion", "value": (data) => { return getEstFinMonth(data); }, "order": 2},
       {"name": "Year Built", "value": (data) => { return getYearBuilt(data); }, "order": 3},
       {"name": "Building", "value": (data) => { return get(data, 'post_meta.rets_building_name[0]', null); }, "order": 4},
       {"name": "Building SQFT", "value": (data) => { return get(data, 'post_meta.rets_building_sq_ft[0]', null); }, "order": 5},
@@ -78,7 +80,7 @@ export default [
       {"name": "SQFT", "value": (data) => { return getSQFT(data); }, 'order': 3},
       {"name": "Dimensions", "value": (data) => { return getLotSizeDim(data); }, 'order': 4},
       {"name": "Zoning", "value": (data) => { return getZoning(data); }, "order": 5},
-      {"name": "Flood Plain", "value": (data) => { return null; }, "order": 6},
+      {"name": "Flood Plain", "value": (data) => { return get(data, 'tax_input.rets_flood_plain.rets_flood_plain', []).map(d => d.name).join(', ') || null; }, "order": 6},
       {"name": "Road Frontage", "value": (data) => { return getRoadFrontage(data); }, "order": 7},
       {"name": "Traffic Count", "value": (data) => { return get(data, 'post_meta.rets_traffic_count[0]', null); }, "order": 8}
     ], "order": 2}
@@ -86,31 +88,31 @@ export default [
   {
     "name": "Location", "children": [
       {"name": "Community", "items": [
-        {"name": "County", "value": (data) => { return getCounty(data); }, "order": 1},
-        {"name": "Area", "value": (data) => { return getArea(data); }, "order": 2},
-        {"name": "Sub Area", "value": (data) => { return getSubArea(data); }, "order": 3},
-        {"name": "Distance to RDU", "value": (data) => { return null; }, "order": 4},
+        {"name": "Inside City", "value": (data) => { return getInsideCity(data); }, "order": 1},
+        {"name": "County", "value": (data) => { return getCounty(data); }, "order": 2},
+        {"name": "Area", "value": (data) => { return getArea(data); }, "order": 3},
+        {"name": "Sub Area", "value": (data) => { return getSubArea(data); }, "order": 4},
+        {"name": "Distance to RDU", "value": (data) => { return get(data, 'tax_input.rets_distance_rdu.rets_distance_rdu', []).map(d => d.name).join(', ') || null; }, "order": 5}
       ], "order": 1},
       {"name": "Address", "items": [
         {"name": "Cross Street", "value": (data) => { return getCrossStreet(data); }, "order": 1},
-        {"name": "Inside City", "value": (data) => { return getInsideCity(data); }, "order": 2},
-        {"name": "Street Number", "value": (data) => { return getStreetNumber(data); }, "order": 3},
-        {"name": "Street Directional", "value": (data) => { return getStreetDirectional(data); }, "order": 4},
-        {"name": "Street", "value": (data) => { return getStreet(data); }, "order": 5},
-        {"name": "Post Directional", "value": (data) => { return null; }, "order": 6},
-        {"name": "Unit Number", "value": (data) => { return getUnitNumber(data); }, "order": 7},
-        {"name": "City", "value": (data) => { return getCity(data); }, "order": 8},
-        {"name": "State", "value": (data) => { return getState(data); }, "order": 9},
-        {"name": "Zip", "value": (data) => { return getZip(data); }, "order": 10},
-        {"name": "Latitude", "value": (data) => { return getLatitude(data); }, "order": 11},
-        {"name": "Longitude", "value": (data) => { return getLongitude(data); }, "order": 12},
-        {"name": "Directions", "value": (data) => { return getDirections(data); }, "order": 13}
+        {"name": "Street Number", "value": (data) => { return getStreetNumber(data); }, "order": 2},
+        {"name": "Street Directional", "value": (data) => { return getStreetDirectional(data); }, "order": 3},
+        {"name": "Street", "value": (data) => { return getStreet(data); }, "order": 4},
+        {"name": "Post Directional", "value": (data) => { return get(data, 'tax_input.rets_post_direction.rets_post_direction', []).map(d => d.name).join(', ') || null; }, "order": 5},
+        {"name": "Unit Number", "value": (data) => { return getUnitNumber(data); }, "order": 6},
+        {"name": "City", "value": (data) => { return getCity(data); }, "order": 7},
+        {"name": "State", "value": (data) => { return getState(data); }, "order": 8},
+        {"name": "Zip", "value": (data) => { return getZip(data); }, "order": 9},
+        {"name": "Latitude", "value": (data) => { return getLatitude(data); }, "order": 10},
+        {"name": "Longitude", "value": (data) => { return getLongitude(data); }, "order": 11},
+        {"name": "Directions", "value": (data) => { return getDirections(data); }, "order": 12  }
       ], "order": 3},
     ]
   },
   {"name": "Listing", "children": [
     {"name": "Pricing", "items": [
-      {"name": "Type", "value": (data) => { return 'Commercial'; }, 'order': 1},
+      {"name": "Type", "value": (data) => { return getListingStatuses(data); }, 'order': 1},
       {"name": "Price", "value": (data) => { return getPrice(data); }, "order": 2},
       {"name": "Price Per SQFT", "value": (data) => { return getPricePerSQFT(data); }, 'order': 3},
       {"name": "Price Per Acre", "value": (data) => { return getPricePerAcre(data); }, "order": 4},
@@ -120,9 +122,9 @@ export default [
       {"name": "Max SQFT Available", "value": (data) => { return get(data, 'post_meta.rets_maximum_sq_ft_available[0]', null); }, "order": 8}
     ], "order": 1},
     {"name": "Terms", "items": [
-      {"name": "Includes", "value": (data) => { return null; }, "order": 1},
-      {"name": "Special Conditions", "value": (data) => { return null; }, "order": 2},
-      {"name": "Restrictions", "value": (data) => { return null; }, "order": 3}
+      {"name": "Includes", "value": (data) => { return get(data, 'tax_input.rets_sale_lease_includes.rets_sale_lease_includes', []).map(d => d.name).join(', ') || null; }, "order": 1},
+      {"name": "Special Conditions", "value": (data) => { return get(data, 'tax_input.rets_special_conditions.rets_special_conditions', []).map(d => d.name).join(', ') || null; }, "order": 2},
+      {"name": "Restrictions", "value": (data) => { return get(data, 'tax_input.rets_restrictions.rets_restrictions', []).map(d => d.name).join(', ') || null; }, "order": 3}
     ], "order": 2},
     {"name": "Status", "items": [
       {"name": "MLS ID", "value": (data) => { return getMLSId(data); }, "order": 1},
