db627891b711956a6452cf807f90f7e4297efad7
RumenTech <rumen.buhalov@gmail.com>
2017-11-24 11:04:40 +0200

    updated interface previews

--- wp-content/themes/wp-property-pro/static/scripts/src/src/components/properties/SearchFilterDescriptionTextPlaceholder.jsx
+++ wp-content/themes/wp-property-pro/static/scripts/src/src/components/properties/SearchFilterDescriptionTextPlaceholder.jsx
@@ -7,12 +7,12 @@ import { Lib } from 'app_root/lib.jsx';
 const Placeholder = () => {
   return (
     <div className={ `${Lib.THEME_CLASSES_PREFIX}headtitle ${Lib.THEME_CLASSES_PREFIX}content-placeholder` }>
-      <h1>
+      <h1 className={`${Lib.THEME_CLASSES_PREFIX}headtitle__title`}>
         <ContentLoader primaryColor={ '#8b8c8d' }>
           <Rect />
         </ContentLoader>
       </h1>
-      <p>
+      <p className={`${Lib.THEME_CLASSES_PREFIX}headtitle__text`}>
         <ContentLoader primaryColor={ '#dcdcdc' }>
           <Rect />
         </ContentLoader>
