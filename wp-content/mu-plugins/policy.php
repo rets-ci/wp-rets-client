80d336e695edf30b226f72d7363d69fa3c9ea86d
pemaxim <maxim.peshkov@usabilitydynamics.com>
2017-12-01 18:49:06 +0200

    fixed the list of required plugins

--- wp-content/mu-plugins/policy.php
+++ wp-content/mu-plugins/policy.php
@@ -34,17 +34,11 @@ namespace RedDoorCompany\Policy {
     $_plugins[] = 'siteorigin-panels/siteorigin-panels.php';
     $_plugins[] = 'so-widgets-bundle/so-widgets-bundle.php';
     $_plugins[] = 'wp-stateless/wp-stateless-media.php';
-
     $_plugins[] = 'wp-property/wp-property.php';
     $_plugins[] = 'wp-property-terms/wp-property-terms.php';
     $_plugins[] = 'wp-property-agents/wp-property-agents.php';
+    $_plugins[] = 'wp-rets-client/wp-rets-client.php';
 
-    $_plugins[] = 'wp-upstream/wp-upstream.php';
-    $_plugins[] = 'wp-upstream/wp-upstream.php';
-    $_plugins[] = 'wp-stateless/wp-stateless-media.php';
-    $_plugins[] = 'wp-rabbit/wp-rabbit.php';
-    $_plugins[] = 'siteorigin-panels/siteorigin-panels.php';
-    $_plugins[] = 'so-widgets-bundle/so-widgets-bundle.php';
 
     return array_unique( $_plugins );
 
