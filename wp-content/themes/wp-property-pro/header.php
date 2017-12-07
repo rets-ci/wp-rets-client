dfc9cbf7caa76c8133896a24c68e03dc0ba23d7b
Eric Sopp <eric.sopp@usabilitydynamics.com>
2017-12-07 14:00:22 -0500

    removed loading font weights we don't use

--- wp-content/themes/wp-property-pro/header.php
+++ wp-content/themes/wp-property-pro/header.php
@@ -7,7 +7,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,500,500i,600,700|Playfair+Display" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,700|Playfair+Display:400" />
     <link rel="manifest" href="/wp-content/themes/wp-property-pro/static/scripts/src/manifest.json">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
