b95152fcf91c91778268e7d737ce1c2f50372f18
fqjony <fq.jony@gmail.com>
2017-12-14 11:38:58 +0200

    remove spacing for head tags

--- wp-content/themes/wp-property-pro/header.php
+++ wp-content/themes/wp-property-pro/header.php
@@ -4,19 +4,19 @@
 ?><!DOCTYPE html>
 <html>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
-    <meta name="format-detection" content="telephone=no">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,700|Playfair+Display:400" />
-    <link rel="manifest" href="/wp-content/themes/wp-property-pro/static/scripts/src/manifest.json">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-    <noscript><p class="no-js-header">This site uses Javascript extensively to work. Please enable Javascript on your browser to continue!</p></noscript>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width">
+<meta name="format-detection" content="telephone=no">
+<title><?php wp_title(); ?></title>
+<?php wp_head(); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,700|Playfair+Display:400" />
+<link rel="manifest" href="/wp-content/themes/wp-property-pro/static/scripts/src/manifest.json">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+<noscript><p class="no-js-header">This site uses Javascript extensively to work. Please enable Javascript on your browser to continue!</p></noscript>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
