e0f948c10cb811bfb7d5adf674946ca0d4e9f004
RumenTech <rumen.buhalov@gmail.com>
2017-12-15 10:53:54 +0200

    load Lato 500 font

--- wp-content/themes/wp-property-pro/header.php
+++ wp-content/themes/wp-property-pro/header.php
@@ -10,7 +10,7 @@
 <title><?php wp_title(); ?></title>
 <?php wp_head(); ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,600,700|Playfair+Display:400" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400i,500,600,700|Playfair+Display:400" />
 <link rel="manifest" href="/wp-content/themes/wp-property-pro/static/scripts/src/manifest.json">
 <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
 <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
