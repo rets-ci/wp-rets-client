b9417b0c24c2d95342ada7ba3f262d6f80e8fd97
fqjony <fq.jony@gmail.com>
2018-10-08 16:54:14 +0300

    updates for rabbit loader connector

--- lib/connectors/class-rabbit-loader.php
+++ lib/connectors/class-rabbit-loader.php
@@ -1,24 +1,25 @@
 <?php
 
 /**
- * Compatibility connector for wp-rabbit
+ * Compatibility connector for rabbit loader
  */
+
 namespace UsabilityDynamics\WPRETSC\Connectors {
 
-  if ( !class_exists( 'UsabilityDynamics\WPRETSC\Connectors\WPRABBIT' ) ) {
+  if( !class_exists( 'UsabilityDynamics\WPRETSC\Connectors\RabbitLoader' ) ) {
 
     /**
-     * Class WPRABBIT
+     * Class RabbitLoader
      * @package UsabilityDynamics\WPRETSC\Connectors
      */
-    final class WPRABBIT {
+    final class RabbitLoader {
 
       /**
-       * WPRABBIT constructor.
+       * RabbitLoader constructor.
        */
       public function __construct() {
 
-        add_action( 'wrc::manage_property::before_update', function() {
+        add_action( 'wrc::manage_property::before_update', function () {
           remove_filter( 'save_post', 'rabbit_save_post_purging_handler', 50 );
         } );
 
